40ac73aabfb33fc6ade1e8b9f094f7485b7d95b8
Night <night@nightdev.com>
2013-04-18 17:53:28 -0400

    Custom counter

--- betterttv.js
+++ betterttv.js
@@ -1207,7 +1207,7 @@ BetterTTVEngine = function() {
 	darkenPage = function() {
 
 		betterttvDebug.log("Darkening Chat");
-		
+
 		if(PP['page_type'] === "video" || PP['page_type'] === "channel") {
 			if(localStorage.getItem("darkchat") === "true") {
 				var darkCSS = document.createElement("link");
@@ -1218,14 +1218,14 @@ BetterTTVEngine = function() {
 				darkCSS.innerHTML = '';
 				$j('body').append(darkCSS);
 
-				$j("channel_viewer_count").css("display","none !important");
-
+				$j("channel_viewer_count").replaceWith('<span class="stat" id="channel_viewer_count_bttv" original-title="Watching Now" style="display:none;"></span>');
+				
 				function setChannelViewerCount(e, t) {
-					if($j("channel_viewer_count").length && e !== 0 && e > 0) {
-						$j("channel_viewer_count").html(commatize(e));
-						$j("channel_viewer_count").show("display","inline-block");
+					if($j("channel_viewer_count_bttv").length && e !== 0 && e > 0) {
+						$j("channel_viewer_count_bttv").html(commatize(e));
+						$j("channel_viewer_count_bttv").show("display","inline-block");
 					} else {
-						$j("channel_viewer_count").css("display","none !important");
+						$j("channel_viewer_count_bttv").css("display","none");
 					}
 				}
 			}
