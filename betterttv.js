35593721b2da464231d03550b6ba905eb27a390a
Night <night@nightdev.com>
2013-08-19 21:46:17 -0400

    6.4.5
    Removed old channel reformat
    Rebranded new channel reformat to channel format
    Added /mods checking
    Fixed Phrase matching for blacklist and highlighting
    Fixed Dashboard /w new layout (New Bug: Double Chat sometimes)

--- betterttv.js
+++ betterttv.js
@@ -44,6 +44,7 @@ BetterTTVEngine = function () {
     /**
      * Helper Functions
      */
+     
     String.prototype.capitalize = function () {
         return this.charAt(0).toUpperCase() + this.slice(1);
     }
