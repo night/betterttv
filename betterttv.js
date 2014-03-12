bc55ef25974cb4b9028391d1b31df2a65ff20abd
Night <night@nightdev.com>
2014-03-12 14:52:34 -0400

    Increment release for new beta push

--- betterttv.js
+++ betterttv.js
@@ -67,7 +67,7 @@
 
     bttv.info = {
         version: "6.7-BETA",
-        release: 14,
+        release: 15,
         versionString: function() {
             return bttv.info.version + 'R' + bttv.info.release;
         }
