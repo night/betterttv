9650fc8439acf571fb46e1ac5ee5f20f810d6f01
Night <night@nightdev.com>
2014-03-28 14:58:57 -0400

    Scroll chat once per second to reduce lag

--- betterttv.js
+++ betterttv.js
@@ -1340,6 +1340,8 @@
                 return specials;
             },
             scrollChat: function() {
+                if(bttv.chat.store.__isScrolling) return;
+                bttv.chat.store.__isScrolling = true;
                 if($('.ember-chat .chat-interface .more-messages-indicator').length || !$('.ember-chat .chat-messages .chat-line').length) return;
                 $('.ember-chat .chat-messages .tse-scroll-content')[0].scrollTop = $('.ember-chat .chat-messages .tse-scroll-content')[0].scrollHeight;
                 var linesToDelete = $('.ember-chat .chat-messages .chat-line').length - bttv.settings.get("scrollbackAmount");
@@ -1349,6 +1351,10 @@
                         $('.ember-chat .chat-messages .chat-line')[0].remove();
                     }
                 }
+
+                setTimeout(function() {
+                    bttv.chat.store.__isScrolling = false;
+                }, 1000);
             },
             ban: function(user) {
                 if(!user || user === "") return false;
@@ -1961,9 +1967,9 @@
             }
         },
         store: {
+            __isScrolling: false,
             __messageTimer: false,
             __messageQueue: [],
-            __messageCount: 0,
             __usersBeingLookedUp: 0,
             __subscriptions: {},
             __unbannedUsers: [],
