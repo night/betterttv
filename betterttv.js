71f5835fe1dda121ea95d50953817722c73289b3
Night <night@nightdev.com>
2014-04-03 21:13:22 -0400

    no message

--- betterttv.js
+++ betterttv.js
@@ -159,7 +159,7 @@
 
     bttv.info = {
         version: "6.7",
-        release: 2,
+        release: 3,
         versionString: function() {
             return bttv.info.version + 'R' + bttv.info.release;
         }
