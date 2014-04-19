c4ae7196cd718ce7ae70e354a6fc18bbdd016997
Night <night@nightdev.com>
2014-04-18 23:33:20 -0400

    Increment release

--- betterttv.js
+++ betterttv.js
@@ -159,7 +159,7 @@
 
     bttv.info = {
         version: "6.7",
-        release: 5,
+        release: 6,
         versionString: function() {
             return bttv.info.version + 'R' + bttv.info.release;
         }
