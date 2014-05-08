7c7f94fef9f96b460cd05c8f42584327ad63ffb5
Dylan Tittel <dylan@dylantittel.com>
2014-05-08 19:57:27 -0400

    First time compiling Node.JS Hope this works right.
    Trial 1

--- betterttv.js
+++ betterttv.js
@@ -592,7 +592,7 @@ vars = require('vars');
 
 bttv.info = {
     version: "6.7",
-    release: 8,
+    release: 9,
     versionString: function() { 
         return bttv.info.version + 'R' + bttv.info.release;
     }
@@ -2694,6 +2694,35 @@ module.exports = [
             }
         }
     },
+
+    {
+        name: 'Hide Private Chat System',
+        description: 'Hides the system used for private chats',
+        default: false,
+        storageKey: 'PrivateChatRemoval',
+        toggle: function(value) {
+            var $ = document;
+            var cssId = 'removePC'; 
+            if(value === true) {
+                if (!$.getElementById(cssId))
+                {
+                    var head  = $.getElementsByTagName('head')[0];
+                    var link  = $.createElement('link');
+                    link.id   = cssId;
+                    link.rel  = 'stylesheet';
+                    link.type = 'text/css';
+                    link.href = '../../style/stylesheet/betterttv-hide-group-chat.css';
+                    head.appendChild(link);
+                }
+            } else {
+                 if ($.getElementById(cssId))
+                {
+                    document.getElementById("cssId").remove();
+                }
+            }
+        }
+    },
+
     {   
         default: '',
         storageKey: 'blacklistKeywords',
