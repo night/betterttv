957b67860c84a2d3403023f5c221d7c38e606ede
Teak Topham <teak42@gmail.com>
2014-08-23 18:22:17 -0700

    6.8R6

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ vars = require('vars');
 
 bttv.info = {
     version: "6.8",
-    release: 5,
+    release: 6,
     versionString: function() { 
         return bttv.info.version + 'R' + bttv.info.release;
     }
