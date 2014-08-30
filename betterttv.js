d88a3c394eefe89d721b925348863e3b3bfd99e5
Night <night@nightdev.com>
2014-08-30 18:57:44 -0400

    Fix beta chat for when users revoke its api permissions

--- betterttv.js
+++ betterttv.js
@@ -2272,11 +2272,11 @@ module.exports = function () {
 
         if(!vars.betaChatLoaded) {
             vars.betaChatLoaded = true;
-            $.getJSON("//chat.betterttv.net/login.php?onsite=true&user="+vars.userData.login+"&callback=?", function(d) {
-
+            $.getJSON("//chat.betterttv.net/login.php?onsite=true&verify=true&callback=?", function(d) {
                 if(d.status === true) {
                     debug.log("Logged into BTTV Chat");
                 } else {
+                    $.getJSON("//chat.betterttv.net/login.php?onsite=true&user="+vars.userData.login+"&callback=?");
                     debug.log("Not logged into BTTV Chat");
                 }
 
@@ -2291,7 +2291,6 @@ module.exports = function () {
                     chatJSInject.setAttribute("type", "text/javascript");
                     $("body").append(chatJSInject);
                 }, 5000);
-
             });
 
             var chatCSSInject = document.createElement("link");
