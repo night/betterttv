26ff395bf67968f0675fb50c9de6e19a2d6cb491
Night <night@nightdev.com>
2014-09-01 01:39:46 -0400

    Double-click to translate supports Twitch's new language selection

--- betterttv.js
+++ betterttv.js
@@ -1297,7 +1297,7 @@ bttv.chat = {
             }
         },
         translate: function(element, sender, text) {
-            var language = window.location.host.split('.')[0].replace(/^(www|beta)$/,"en"),
+            var language = (window.cookie && window.cookie.get('language')) ? window.cookie.get('language') : 'en',
                 query = 'http://translate.google.com/translate_a/t?client=bttv&sl=auto&tl='+language+'&ie=UTF-8&oe=UTF-8&q='+text,
                 translate = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D\""+encodeURIComponent(query)+"\"&format=json&diagnostics=false&callback=?";
 
