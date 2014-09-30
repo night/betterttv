ed4d9df01b93a3749a41077d79c3b9b3644d222d
Night <night@nightdev.com>
2014-09-30 14:59:40 -0400

    Fix directory live tab auto-clicking

--- src/features/directory-functions.js
+++ src/features/directory-functions.js
@@ -2,10 +2,10 @@ var debug = require('../debug'),
     vars = require('../vars');
 
 module.exports = function () {
-    if(bttv.settings.get("showDirectoryLiveTab") === true && $('h2.title:contains("Channels You Follow")').length && $('a.active:contains("Overview")').length) {
+    if(bttv.settings.get("showDirectoryLiveTab") === true && $('h2.title:contains("Following")').length && $('a.active:contains("Overview")').length) {
         debug.log("Changing Directory View");
 
-        $('a:contains("Live Channels")').click();
+        $('a:contains("Channels")').click();
     }
 
     if(vars.watchScroll) return;
