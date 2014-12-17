03010ed15e5c37f8ff76518a024674e09446fa3b
Night <night@nightdev.com>
2014-12-16 23:36:38 -0500

    Finish resizer for Theater mode compatibility

--- betterttv.js
+++ betterttv.js
@@ -2472,7 +2472,32 @@ var handleResize = module.exports = function () {
         $('<style></style>').attr('id', 'bttvPlayerStyle').appendTo('body');
     }
 
-    $('#bttvPlayerStyle').html('#player, .dynamic-player, .dynamic-player object, .dynamic-player video, #hostmode .target-player object { width: 100% !important; }');
+    var fullPlayerHeight = ($('#player object').width() * 0.5625) + 30;
+
+    var metaAndStatsHeight = $('#broadcast-meta').outerHeight(true) + $('.stats-and-actions').outerHeight();
+
+    var desiredPageHeight = metaAndStatsHeight + fullPlayerHeight;
+
+    if($(window).height() > desiredPageHeight) {
+        $('#bttvPlayerStyle').html('.dynamic-player, .dynamic-target-player, .dynamic-player object, .dynamic-player video, .dynamic-target-player object { width: 100% !important; height: '+fullPlayerHeight+'px !important; }');
+    } else {
+        var playerHeight = $(window).height() - metaAndStatsHeight;
+
+        $('#bttvPlayerStyle').html('.dynamic-player, .dynamic-target-player, .dynamic-player object, .dynamic-player video, .dynamic-target-player object { width: 100% !important; height: '+playerHeight+'px !important; }');
+    }
+
+    if($('#hostmode').length) {
+        var h = 0.5625 * $("#main_col").width() - 4;
+        var calcH = $(window).height() - $(".hostmode-title-container").outerHeight(true) - $(".target-meta").outerHeight(true) - $("#hostmode .channel-actions").outerHeight(true) - $(".close-hostmode").outerHeight(true) - 33;
+
+        if(h > calcH) {
+            $('#bttvPlayerStyle').html('.dynamic-player, .dynamic-target-player, .dynamic-player object, .dynamic-player video, .dynamic-target-player object { width: 100% !important; height: '+ calcH + 'px !important; }');
+        } else {
+            $('#bttvPlayerStyle').html('.dynamic-player, .dynamic-target-player, .dynamic-player object, .dynamic-player video, .dynamic-target-player object { width: 100% !important; height: '+ h.toFixed(0) + 'px !important; }');
+        }
+
+        $('.target-frame').css('height',$(window).height());
+    }
 
     $("#channel_panels_contain").masonry("reload");
 };
