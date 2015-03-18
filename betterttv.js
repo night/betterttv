bc88bca8301fd4486c04d1e26c5c225b1f8732da
Night <night@nightdev.com>
2015-03-18 14:01:12 -0400

    Fix emote menu

--- betterttv.js
+++ betterttv.js
@@ -1112,10 +1112,10 @@ bttv.chat = {
             var emote = emotes[key];
 
             if(emote.restrictions) {
-                if(emote.restrictions.channels.length && emote.restrictions.channels.indexOf(bttv.getChannel()) === -1) return message;
-                if(emote.restrictions.games.length && bttv.chat.tmi().channel && emote.restrictions.games.indexOf(bttv.chat.tmi().channel.game) === -1) return message;
+                if(emote.restrictions.channels.length && emote.restrictions.channels.indexOf(bttv.getChannel()) === -1) return;
+                if(emote.restrictions.games.length && bttv.chat.tmi().channel && emote.restrictions.games.indexOf(bttv.chat.tmi().channel.game) === -1) return;
 
-                if(emote.restrictions.emoticonSet && emoteSets.indexOf(emote.restrictions.emoticonSet) === -1) return message;
+                if(emote.restrictions.emoticonSet && emoteSets.indexOf(emote.restrictions.emoticonSet) === -1) return;
             }
 
             emote.text = emote.code;
