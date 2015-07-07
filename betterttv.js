c694c1652d2bd7c1e768f1021f3df56a2835630e
Night <night@nightdev.com>
2015-07-06 21:00:33 -0400

    Fixes #332

--- betterttv.js
+++ betterttv.js
@@ -3841,26 +3841,20 @@ module.exports = function() {
 var debug = require('../helpers/debug');
 
 var enablePreview = exports.enablePreview = function() {
-    /* CONFIG */
-    var xOffset = -255,
-        yOffset = 0;
-
-    /* END CONFIG */
     $(document).on({
-        mouseenter: function (e) {
-            $("body").append('<iframe id="chat_preview" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" width="250px" scrolling="no" src="https://api.betterttv.net/2/image_embed/'+ encodeURIComponent(this.href) +'"></iframe>');
-            $("#chat_preview")
-                .css("top",(e.pageY - yOffset) + "px")
-                .css("left", (e.pageX - xOffset) + "px")
-                .css("position", "absolute")
-                .css("z-index", '100')
-                .fadeIn("fast");
-        }, mouseleave: function (e) {
-            $("#chat_preview").remove();
-        }, mousemove: function (e) {
-            $("#chat_preview")
-            .css("top",(e.pageY - yOffset) + "px")
-            .css("left",(e.pageX + xOffset) + "px");
+        mouseenter: function(e) {
+            var url = this.href;
+
+            $(this).tipsy({
+                trigger: 'manual',
+                gravity: $.fn.tipsy.autoNS,
+                html: true,
+                title: function() { return '<iframe id="chat_preview" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" width="200px" scrolling="no" src="https://api.betterttv.net/2/image_embed/'+ encodeURIComponent(url) +'"></iframe>'; }
+            });
+            $(this).tipsy("show");
+        }, mouseleave: function(e) {
+            $(this).tipsy("hide");
+            $('div.tipsy').remove();
         }
     }, 'a.chat-preview');
 };
