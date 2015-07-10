8b19d9b6a749b3299f7984169e46c8b22995c291
Night <night@nightdev.com>
2015-07-10 19:24:44 -0400

    6.8R39

--- betterttv.js
+++ betterttv.js
@@ -2313,7 +2313,7 @@ var debug = require('./helpers/debug'),
 
 bttv.info = {
     version: "6.8",
-    release: 38,
+    release: 39,
     versionString: function() {
         return bttv.info.version + 'R' + bttv.info.release;
     }
--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ var debug = require('./helpers/debug'),
 
 bttv.info = {
     version: "6.8",
-    release: 38,
+    release: 39,
     versionString: function() {
         return bttv.info.version + 'R' + bttv.info.release;
     }
