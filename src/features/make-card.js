61deced253be8d47d5c83f736727a50e959d3a0d
teak <teaksoftware@gmail.com>
2015-07-18 22:27:30 -0700

    fix mod card location

--- src/features/make-card.js
+++ src/features/make-card.js
@@ -2,7 +2,7 @@ module.exports = function(user, $event) {
     // adds in user messages from chat
     user.messages = $.makeArray($('.chat-container .chat-messages .chat-line[data-sender="'+user.name+'"]')).reverse();
 
-    var template = bttv.chat.templates.moderationCard(user, $event.offset().top, $('.chat-line:last').offset().left);
+    var template = bttv.chat.templates.moderationCard(user, $event.offset().top, $('.chat-lines').offset().left);
     $('.ember-chat .moderation-card').remove();
     $('.ember-chat').append(template);
 
