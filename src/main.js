4b6c4e338d9e7c91dc0f805d45d08d568124381d
Night <night@nightdev.com>
2015-08-10 01:10:35 -0400

    6.8R42

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ var debug = require('./helpers/debug'),
 
 bttv.info = {
     version: '6.8',
-    release: 41,
+    release: 42,
     versionString: function() {
         return bttv.info.version + 'R' + bttv.info.release;
     }
