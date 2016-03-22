e6edfbd2c62a277277d267b00e625a3cbc6a0c87
night <night@nightdev.com>
2016-03-22 18:49:00 -0400

    fixes auto theater mode

--- src/features/auto-theatre-mode.js
+++ src/features/auto-theatre-mode.js
@@ -6,6 +6,5 @@ module.exports = function() {
 
     var emberView = $('#player').children()[0].id;
     var emberViews = App.__container__.lookup('-view-registry:main');
-    var player = window.require('web-client/components/twitch-player2').getPlayer();
-    emberViews[emberView].sendAction('toggleTheatreAction', player);
+    emberViews[emberView].sendAction('toggleTheatreAction', emberViews[emberView].get('player'));
 };
