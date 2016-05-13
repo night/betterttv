8d94a7fc560d5407f9c0e50396d71efcdf92dd81
wobbly <WobblyToddler@users.noreply.github.com>
2016-05-13 13:42:11 +0300

    Hide only the recommeded friends

--- src/features/css-loader.js
+++ src/features/css-loader.js
@@ -2,7 +2,7 @@ function load(file, key) {
     if (!bttv.settings.get(key)) return;
 
     var css = document.createElement('link');
-    css.setAttribute('href', 'https://cdn.betterttv.net/style/stylesheets/betterttv-' + file + '.css?' + bttv.info.versionString());
+    css.setAttribute('href', 'https://localhost/style/stylesheets/betterttv-' + file + '.css?' + bttv.info.versionString());
     css.setAttribute('type', 'text/css');
     css.setAttribute('rel', 'stylesheet');
     css.setAttribute('id', key);
