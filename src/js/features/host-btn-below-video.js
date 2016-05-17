24f37711409e4caa65892d7d705f33839e067df4
Ehsan Kia <contact@ehsankia.com>
2016-05-16 23:28:46 -0400

    Fix selector for host button (#1172)

--- src/js/features/host-btn-below-video.js
+++ src/js/features/host-btn-below-video.js
@@ -20,7 +20,7 @@ module.exports = function() {
         $hostButton = $('<span><span></span></span>');
         $hostButton.addClass('button').addClass('action');
         $hostButton.attr('id', 'bttv-host-button');
-        $hostButton.insertBefore('#channel .channel-actions .js-options');
+        $hostButton.insertBefore('#channel .channel-actions > span:eq(2)');
         $hostButton.click(function() {
             var action = $hostButton.text();
 
