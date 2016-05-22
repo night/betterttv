894ac758ff255ed21713fa31c472946ef9530633
Kirill Voloshin <voloshin.k@husky.neu.edu>
2016-05-22 01:13:27 -0400

    Only convert to number of indexOf check
    Saving that 0.00001ms

--- src/js/chat/templates.js
+++ src/js/chat/templates.js
@@ -74,9 +74,8 @@ var jtvEmoticonize = exports.jtvEmoticonize = function(id) {
 
 var emoticon = exports.emoticon = function(id, name) {
     var fixedTwitchEmotes = [15, 16, 17, 18, 19, 20, 21, 22, 26, 27, 33];
-    id = parseInt(id, 10);
 
-    if (fixedTwitchEmotes.indexOf(id) > -1) {
+    if (fixedTwitchEmotes.indexOf(parseInt(id, 10)) > -1) {
         return '<img class="emoticon ttv-emo-' + id + ' "src="https://cdn.betterttv.net/emotes/twitch_fixed/' + id + '_1x.png" srcset="https://cdn.betterttv.net/emotes/twitch_fixed/' + id + '_2x.png 2x" data-id="' + id + '" alt="' + escapeEmoteCode(name) + '" />';
     }
     if (id < 15 && bttv.settings.get('showMonkeyEmotes') === true) {
