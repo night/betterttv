0db9fb108cfca92236a5acba1e5614dc15ae9164
Ehsan Kia <contact@ehsankia.com>
2016-06-05 22:53:17 -0400

    Multiple fixes (#1241)

--- src/js/features/auto-theatre-mode.js
+++ src/js/features/auto-theatre-mode.js
@@ -1,10 +1,8 @@
 module.exports = function() {
-    if (!window.Ember || !window.App ||
-        App.__container__.lookup('controller:application').get('currentRouteName') !== 'channel.index.index') {
-        return;
-    }
+    if (!window.Ember || !window.App) return;
 
-    var emberView = $('#player').children()[0].id;
-    var emberViews = App.__container__.lookup('-view-registry:main');
-    emberViews[emberView].sendAction('toggleTheatreAction', emberViews[emberView].get('player'));
+    var routeName = App.__container__.lookup('controller:application').get('currentRouteName');
+    if (routeName !== 'channel.index.index' && routeName !== 'vod') return;
+
+    App.__container__.lookup('controller:channel').send('toggleTheatre');
 };
