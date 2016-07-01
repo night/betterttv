08509b793d89f5971e67b21d544d3d412ee8069d
Aedan Dispenza <aedan.dispenza@gmail.com>
2016-07-01 11:21:54 -0400

    fixed binding bugs

--- src/js/features/live-viewers.js
+++ src/js/features/live-viewers.js
@@ -1,18 +1,20 @@
 var debug = require('../helpers/debug');
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > milliseconds) {
-            break;
+function checkBind() {
+    $.each($('.live-count').data('events'), function(i) {
+        if (i === 'DOMSubtreeModified') {
+            return true;
         }
-    }
+    });
+    return false;
 }
+
 
 module.exports = function() {
     if (bttv.settings.get('showViewersInPlayer') === true) {
-        sleep(1000);
-        $('.live-count').unbind('DOMSubtreeModified');
+        if (checkBind === true) {
+            $('.live-count').unbind('DOMSubtreeModified');
+        }
         debug.log('Replacing "LIVE" with viewer count');
         $('span.js-live-label').text(function() {
             return $('.live-count').text() + 'Viewers';
@@ -26,7 +28,9 @@ module.exports = function() {
 
     if (bttv.settings.get('showViewersInPlayer') === false) {
         debug.log('Restoring "LIVE" in the player');
-        $('.live-count').unbind('DOMSubtreeModified');
+        if (checkBind === true) {
+            $('.live-count').unbind('DOMSubtreeModified');
+        }
         $('span.js-live-label').text(function() {
             return 'Live';
         });
