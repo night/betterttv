303138283516ad3415db26af029eca82bd23fa78
gabranches <gabranches@gmail.com>
2016-08-19 23:23:15 -0400

    added spam test

--- tests/spamTest.js
+++ tests/spamTest.js
@@ -7,12 +7,14 @@ var msg2 = 'vddddddddddddddddd';
 var msg3 = 'vdvddddddddd';
 var msg4 = 'Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online SwiftRage Play Online Swif';
 var msg5 = 'This game is so boring ResidentSleeper ResidentSleeper    ResidentSleeper ResidentSleeper ResidentSleeper ResidentSleeper ResidentSleeper ResidentSleeper';
+var msg6 = 'MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK MOD GREEK';
 
 assert.deepEqual(spam(msg1), 'd');
 assert.deepEqual(spam(msg2), 'd');
 assert.deepEqual(spam(msg3), 'd');
 assert.deepEqual(spam(msg4), 'Play Online SwiftRage');
 assert.deepEqual(spam(msg5), 'ResidentSleeper');
+assert.deepEqual(spam(msg6), 'MOD GREEK');
 
 
 
