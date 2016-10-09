fbe5ee1bd8343370809ca8ebc2d516f6a9dc4579
Teak <teak@users.noreply.github.com>
2016-10-08 21:38:01 -0700

    fixes #1623 (clips page) (#1629)

--- src/js/features/chat-freeze.js
+++ src/js/features/chat-freeze.js
@@ -1,21 +1,26 @@
 var helpers = require('../chat/helpers');
 
 var shouldFreeze = false;
-
-$('body').on('keydown.chat-freeze', function(e) {
-    if ((e.metaKey || e.ctrlKey) && ($('.chat-room:hover').length !== 0)) {
-        shouldFreeze = true;
-    }
-});
-
-$('body').on('keyup.chat-freeze', function(e) {
-    if (!(e.metaKey || e.ctrlKey) && shouldFreeze) {
-        shouldFreeze = false;
-        helpers.scrollChat();
-        $('.chat-room .chat-interface .more-messages-indicator').click();
-    }
-});
+var isLoaded = false;
 
 module.exports = function() {
+    if (!isLoaded) {
+        $('body').on('keydown.chat-freeze', function(e) {
+            if ((e.metaKey || e.ctrlKey) && ($('.chat-room:hover').length !== 0)) {
+                shouldFreeze = true;
+            }
+        });
+
+        $('body').on('keyup.chat-freeze', function(e) {
+            if (!(e.metaKey || e.ctrlKey) && shouldFreeze) {
+                shouldFreeze = false;
+                helpers.scrollChat();
+                $('.chat-room .chat-interface .more-messages-indicator').click();
+            }
+        });
+
+        isLoaded = true;
+    }
+
     return shouldFreeze;
 };
