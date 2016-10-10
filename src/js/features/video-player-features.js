489083c3d8903846defe653c2bcf4f06269e06db
Night <night@nightdev.com>
2016-10-09 21:33:45 -0700

    fixes double clicking on player

--- src/js/features/video-player-features.js
+++ src/js/features/video-player-features.js
@@ -1,6 +1,11 @@
 module.exports = function() {
     // Click to play/pause video
+    var clicks = 0;
     $('body').on('click', '.player-overlay', function() {
-        $('.js-control-playpause-button').click();
+        clicks++;
+        setTimeout(function() {
+            if (clicks === 1) $('.js-control-playpause-button').click();
+            clicks = 0;
+        }, 250);
     });
 };
