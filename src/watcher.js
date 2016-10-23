d6da160fd5f4ad52362a27626cb8e3e48a7ccdde
Night <night@nightdev.com>
2016-10-23 11:59:52 -0700

    tries to wait for page rendering to complete before firing loaded events

--- src/watcher.js
+++ src/watcher.js
@@ -22,44 +22,66 @@ class Watcher extends EventEmitter {
     routeObserver() {
         if (!window.App || !window.Ember) return;
 
+        let renderingCounter = 0;
+        const waitForLoad = () => {
+            return new Promise(resolve => {
+                setTimeout(() => resolve(renderingCounter === 0 ? null : waitForLoad), 25);
+            });
+        };
+
         const onRouteChange = data => {
-            debug.log('New route: ' + data.currentRouteName);
+            if (data.currentRouteName === route) return;
 
             const lastRoute = route;
             route = data.currentRouteName;
 
-            // trigger on all loads (like resize functions)
-            this.emit('load');
+            debug.log(`New route: ${route}`);
 
-            switch (route) {
-                case 'loading':
-                    break;
+            waitForLoad().then(() => {
+                debug.log(`Route loaded: ${route}`);
 
-                case 'channel.videos.video-type':
-                case 'channel.followers':
-                case 'channel.following':
-                case 'channel.index.index':
-                    this.emit('load.channel');
-                    // Switching between tabs in channel page
-                    if (lastRoute.substr(0, 8) === 'channel.') break;
-                    this.emit('load.chat');
-                    break;
-                case 'vod':
-                    this.emit('load.vod');
-                    this.emit('load.chat');
-                    break;
-                case 'directory.following.index':
-                    // Switching between tabs in following page
-                    if (lastRoute.substr(0, 19) === 'directory.following') break;
-                    this.emit('load.directory.following');
-                    break;
-            }
+                // trigger on all loads (like resize functions)
+                this.emit('load');
+
+                switch (route) {
+                    case 'loading':
+                        break;
+
+                    case 'channel.videos.video-type':
+                    case 'channel.followers':
+                    case 'channel.following':
+                    case 'channel.index.index':
+                        this.emit('load.channel');
+                        // Switching between tabs in channel page
+                        if (lastRoute.substr(0, 8) === 'channel.') break;
+                        this.emit('load.chat');
+                        break;
+                    case 'vod':
+                        this.emit('load.vod');
+                        this.emit('load.chat');
+                        break;
+                    case 'directory.following.index':
+                        // Switching between tabs in following page
+                        if (lastRoute.substr(0, 19) === 'directory.following') break;
+                        this.emit('load.directory.following');
+                        break;
+                }
+            });
         };
 
         const applicationController = window.App.__container__.lookup('controller:application');
 
         onRouteChange({currentRouteName: applicationController.get('currentRouteName')});
         applicationController.addObserver('currentRouteName', onRouteChange);
+
+        Ember.subscribe('render', {
+            before: function() {
+                renderingCounter++;
+            },
+            after: function() {
+                renderingCounter--;
+            }
+        });
     }
 
     chatObserver() {
