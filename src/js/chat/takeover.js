b516c541142fc4bac2854b00d0d53ebfed2a6b12
Ehsan Kia <contact@ehsankia.com>
2016-12-03 18:54:29 -0800

    Fix pinned cheer dismiss button, closes #1805

--- src/js/chat/takeover.js
+++ src/js/chat/takeover.js
@@ -279,7 +279,7 @@ var takeover = module.exports = function() {
     // Dismiss pinned cheers
     $('body').off('click', '.pinned-cheers').on('click', '.pinned-cheers', function(e) {
         if (!e.target.classList.contains('pinned-cheers')) return;
-        if (e.target.offsetWidth - e.offsetX < 48 && e.target.offsetHeight - e.offsetY > 88) $('.pinned-cheers').hide();
+        if (e.target.offsetWidth - e.offsetX < 48 && e.target.offsetHeight - e.offsetY > 48) $('.pinned-cheers').hide();
     });
 
     // Make names clickable
