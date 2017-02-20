a1dabcdebd4a053bcded6495b1e607b8798968b9
Night <night@nightdev.com>
2017-02-19 23:53:35 -0800

    handle edge cases caused by front page being ember

--- src/modules/global_css/index.js
+++ src/modules/global_css/index.js
@@ -1,13 +1,24 @@
 const $ = require('jquery');
 const debug = require('../../utils/debug');
+const watcher = require('../../watcher');
 
 class GlobalCSSModule {
     constructor() {
+        this.globalCSS();
+        this.branding();
+        watcher.on('load', () => this.branding());
+    }
+
+    globalCSS() {
         const globalCSSInject = document.createElement('link');
         globalCSSInject.setAttribute('href', `https://cdn.betterttv.net/css/betterttv.css?${debug.version}`);
         globalCSSInject.setAttribute('type', 'text/css');
         globalCSSInject.setAttribute('rel', 'stylesheet');
         $('body').append(globalCSSInject);
+    }
+
+    branding() {
+        if ($('#bttv_logo').length) return;
 
         const $watermark = $('<img />');
         $watermark.attr('id', 'bttv_logo');
