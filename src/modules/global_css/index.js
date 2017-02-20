f310ab59f93ed7a21ba17687808bd4422068591d
Night <night@nightdev.com>
2017-02-19 16:46:10 -0800

    directory features

--- src/modules/global_css/index.js
+++ src/modules/global_css/index.js
@@ -8,6 +8,17 @@ class GlobalCSSModule {
         globalCSSInject.setAttribute('type', 'text/css');
         globalCSSInject.setAttribute('rel', 'stylesheet');
         $('body').append(globalCSSInject);
+
+        const $watermark = $('<img />');
+        $watermark.attr('id', 'bttv_logo');
+        $watermark.attr('src', 'https://cdn.betterttv.net/assets/logos/logo_icon.png');
+        $watermark.css({
+            'z-index': 9000,
+            'left': '90px',
+            'top': '-10px',
+            'position': 'absolute'
+        });
+        $('.warp .warp__logo').append($watermark);
     }
 }
 
