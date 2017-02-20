9d5a7db1c4c7c41f388cee58c1cab2c4dea9225a
Night <night@nightdev.com>
2017-02-20 13:34:00 -0800

    disable host mode, localized names, name colors

---
+++ src/modules/disable_localized_names/index.js
@@ -0,0 +1,26 @@
+const settings = require('../../settings');
+const watcher = require('../../watcher');
+
+class DisableLocalizedNamesModule {
+    constructor() {
+        settings.add({
+            id: 'disableLocalizedNames',
+            name: 'Disable Localized Names',
+            defaultValue: false,
+            description: 'Show usernames instead of localized names in chat'
+        });
+        watcher.on('chat.message', ($el) => this.delocalizeName($el));
+    }
+
+    delocalizeName($el) {
+        if (settings.get('disableLocalizedNames') === false) return;
+
+        const $name = $el.find('.from');
+        const $login = $name.find('.intl-login');
+        if (!$login.length) return;
+
+        $name.text($login.text().replace(/[\(\))]/g, ''));
+    }
+}
+
+module.exports = new DisableLocalizedNamesModule();
