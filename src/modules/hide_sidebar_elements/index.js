c489eb33884a0aab6f3846293d16a957c9d910dd
Night <night@nightdev.com>
2017-02-20 18:40:12 -0800

    prime promotions

---
+++ src/modules/hide_sidebar_elements/index.js
@@ -0,0 +1,44 @@
+const $ = require('jquery');
+const watcher = require('../../watcher');
+const settings = require('../../settings');
+
+class HideSidebarElementsModule {
+    constructor() {
+        settings.add({
+            id: 'hideFeaturedChannels',
+            name: 'Hide Recommended Channels',
+            defaultValue: true,
+            description: 'The left sidebar is too cluttered, so you can remove recommended channels'
+        });
+        settings.add({
+            id: 'hidePrimePromotion',
+            name: 'Hide Prime Promotions',
+            defaultValue: false,
+            description: 'Hides the "Free With Prime" section of the sidebar'
+        });
+        settings.on('changed.hideFeaturedChannels', () => this.toggleFeaturedChannels());
+        settings.on('changed.hidePrimePromotion', () => this.togglePrimePromotions());
+        watcher.on('load', () => {
+            this.toggleFeaturedChannels();
+            this.togglePrimePromotions();
+        });
+    }
+
+    toggleFeaturedChannels() {
+        if (settings.get('hideFeaturedChannels') === false) {
+            $('.js-recommended-channels').show();
+        } else {
+            $('.js-recommended-channels').hide();
+        }
+    }
+
+    togglePrimePromotions() {
+        if (settings.get('hidePrimePromotion') === false) {
+            $('.js-offers').show();
+        } else {
+            $('.js-offers').hide();
+        }
+    }
+}
+
+module.exports = new HideSidebarElementsModule();
