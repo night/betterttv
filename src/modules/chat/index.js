f07bcb4ef381a9e6e98addd537bf0a2bc2d9bf3d
Night <night@nightdev.com>
2017-02-21 01:29:30 -0800

    update todo

--- src/modules/chat/index.js
+++ src/modules/chat/index.js
@@ -4,8 +4,6 @@ const colors = require('../../utils/colors');
 const settings = require('../../settings');
 
 /* TODO:
- - Chat image preview
- - Link unfurl
  - Emoji
  - Emotes
  - Mention mod cards
@@ -15,8 +13,6 @@ const settings = require('../../settings');
  - Custom mod cards
  - Alpha chat badges?
  - Tab completion?
- - Disable localized names
- - Disable name colors
 */
 
 class ChatModule {
