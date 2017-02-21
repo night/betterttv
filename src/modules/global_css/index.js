f7352854571998f19ecabc8d98454b5ba59f1b4c
Ehsan Kia <contact@ehsankia.com>
2017-02-20 23:41:46 -0800

    bvl & bugfixes (#2081)
    * Optimize color calculation and put it in colors util
    
    * Port BetterViewerList module
    
    * Small theatre mode dark css fix
    
    * Use Twitch jQuery for draggable element
    
    * Move dark_css inside global_css module for css ordering
    
    * Wrap module loading in try catch
    
    * Update TODO
    
    * Cleanup
    
    * cleanup2
    
    * cleanup3
    
    * bvl arrow functions
    
    * swap to map, limit stored colors to 1000

--- src/modules/global_css/index.js
+++ src/modules/global_css/index.js
@@ -1,11 +1,17 @@
 const $ = require('jquery');
 const cdn = require('../../utils/cdn');
 const css = require('../../utils/css');
-const watcher = require('../../watcher');
+const debug = require('../../utils/debug');
 const settings = require('../../settings');
+const watcher = require('../../watcher');
 
 class GlobalCSSModule {
     constructor() {
+        this.globalCSS();
+
+        watcher.on('load', () => this.branding());
+        this.branding();
+
         settings.add({
             id: 'leftSideChat',
             name: 'Left Side Chat',
@@ -13,10 +19,44 @@ class GlobalCSSModule {
             description: 'Moves the chat to the left of the player'
         });
         settings.on('changed.leftSideChat', () => this.toggleLeftSideChat());
-        watcher.on('load', () => this.branding());
-        this.globalCSS();
-        this.branding();
         this.toggleLeftSideChat();
+
+        settings.add({
+            id: 'darkenedMode',
+            name: 'Dark Theme',
+            defaultValue: false,
+            description: 'A sleek, grey theme which will make you love the site even more'
+        });
+        settings.on('changed.darkenedMode', value => value === true ? this.loadDark() : this.unloadDark());
+        this.loadDark();
+    }
+
+    loadDark() {
+        if (settings.get('darkenedMode') !== true || !$('body').attr('data-page')) return;
+
+        const pageKind = $('body').data('page').split('#')[0];
+        const allowedPages = ['ember', 'message', 'chat', 'user'];
+
+        if (allowedPages.indexOf(pageKind) !== -1) {
+            const darkCSS = document.createElement('link');
+            darkCSS.setAttribute('href', `https://cdn.betterttv.net/css/betterttv-dark.css?${debug.version}`);
+            darkCSS.setAttribute('type', 'text/css');
+            darkCSS.setAttribute('rel', 'stylesheet');
+            darkCSS.setAttribute('id', 'bttvDarkTwitch');
+            $('body').append(darkCSS);
+
+            // Messages Delete Icon Fix (Old Messages Inbox)
+            $('#main_col .messages img[src="http://www-cdn.jtvnw.net/images/xarth/g/g18_trash-00000080.png"]')
+                .attr('src', 'https://cdn.betterttv.net/assets/icons/delete.png');
+            $('#main_col .messages img[src="http://www-cdn.jtvnw.net/images/xarth/g/g16_trash-00000020.png"]')
+                .attr('src', 'https://cdn.betterttv.net/assets/icons/delete.png')
+                .attr('width', '16')
+                .attr('height', '16');
+        }
+    }
+
+    unloadDark() {
+        $('#bttvDarkTwitch').remove();
     }
 
     globalCSS() {
