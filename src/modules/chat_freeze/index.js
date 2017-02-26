6c91a52bec1acda06dece9902f625f8fabf5fa92
Night <night@nightdev.com>
2017-02-25 18:31:05 -0800

    chat freeze

---
+++ src/modules/chat_freeze/index.js
@@ -0,0 +1,30 @@
+const CHAT_LINES = '.ember-chat .chat-messages .chat-lines';
+const CHAT_SCROLLER = '.ember-chat .chat-messages .tse-scroll-content';
+
+let isFrozen = false;
+
+function setScrollState(enabled) {
+    const $chatLines = jQuery(CHAT_LINES);
+    $chatLines.css('padding-bottom', enabled ? 0 : 50);
+    const $chatScroller = jQuery(CHAT_SCROLLER);
+    $chatScroller[0].scrollTop = $chatScroller[0].scrollHeight - (enabled ? 0 : 469);
+    $chatScroller.trigger('mousewheel');
+}
+
+class ChatFreezeModule {
+    constructor() {
+        $('body')
+            .on('keydown.chat-freeze', e => {
+                if (!(e.metaKey || e.ctrlKey) || !$(`${CHAT_LINES}:hover`).length) return;
+                isFrozen = true;
+                setScrollState(false);
+            })
+            .on('keyup.chat-freeze', e => {
+                if (e.metaKey || e.ctrlKey || !isFrozen) return;
+                isFrozen = false;
+                setScrollState(true);
+            });
+    }
+}
+
+module.exports = new ChatFreezeModule();
