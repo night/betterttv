8a3d221f3741a5d21386c93f25c4aafe56819650
Night <night@nightdev.com>
2017-02-26 21:26:29 -0800

    personal emotes

---
+++ src/socket-client.js
@@ -0,0 +1,115 @@
+const EventEmitter = require('events').EventEmitter;
+const debug = require('./utils/debug');
+const twitch = require('./utils/twitch');
+
+const CONNECTION_STATES = {
+    DISCONNECTED: 0,
+    CONNECTING: 1,
+    CONNECTED: 2
+};
+
+const WEBSOCKET_ENDPOINT = 'wss://sockets.betterttv.net/ws';
+
+let socket;
+let state = CONNECTION_STATES.DISCONNECTED;
+let attempts = 1;
+
+const joinedChannels = [];
+
+class SocketClient extends EventEmitter {
+    constructor() {
+        super();
+
+        this.connect();
+    }
+
+    connect() {
+        if (state !== CONNECTION_STATES.DISCONNECTED) return;
+        state = CONNECTION_STATES.CONNECTING;
+
+        debug.log('SocketClient: Connecting to server');
+
+        socket = new WebSocket(WEBSOCKET_ENDPOINT);
+
+        socket.onopen = () => {
+            debug.log('SocketClient: Connected to server');
+
+            state = CONNECTION_STATES.CONNECTED;
+            attempts = 1;
+
+            if (joinedChannels.length) {
+                joinedChannels.forEach(c => this.joinChannel(c));
+            }
+        };
+
+        socket.onerror = socket.onclose = () => {
+            if (state === CONNECTION_STATES.DISCONNECTED) return;
+
+            debug.log('SocketClient: Disconnected from server');
+
+            attempts++;
+            this.reconnect();
+        };
+
+        socket.onmessage = ({data}) => {
+            let evt;
+
+            try {
+                evt = JSON.parse(data);
+            } catch (e) {
+                debug.log('SocketClient: Error parsing message', e);
+                return;
+            }
+
+            debug.log('SocketClient: Received event', evt);
+
+            this.emit(evt.name, evt.data);
+        };
+    }
+
+    reconnect() {
+        if (state === CONNECTION_STATES.CONNECTING) return;
+        state = CONNECTION_STATES.DISCONNECTED;
+
+        if (socket) {
+            try {
+                socket.close();
+            } catch (e) {}
+            socket = null;
+        }
+
+        setTimeout(
+            () => this.connect(),
+            Math.random() * (Math.pow(2, attempts) - 1) * 30000
+        );
+    }
+
+    send(name, data) {
+        if (state !== CONNECTION_STATES.CONNECTED) return;
+
+        socket.send(JSON.stringify({
+            name,
+            data
+        }));
+    }
+
+    joinChannel(name) {
+        if (!joinedChannels.includes(name)) {
+            joinedChannels.push(name);
+        }
+        this.send('join_channel', {name});
+        this.broadcastMe(name);
+    }
+
+    partChannel(name) {
+        if (!joinedChannels.includes(name)) return;
+        joinedChannels.splice(joinedChannels.indexOf(name), 1);
+        this.send('part_channel', {name});
+    }
+
+    broadcastMe(channelName) {
+        twitch.getCurrentUser().then(({name}) => this.send('broadcast_me', {name, channel: channelName}));
+    }
+}
+
+module.exports = new SocketClient();
