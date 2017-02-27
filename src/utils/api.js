e54fe3cbc0dfba2e6b6150ab8f37a033b9a587f1
Night <night@nightdev.com>
2017-02-26 18:09:25 -0800

    channel tips & emotes base

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,43 @@
 const $ = require('jquery');
+const querystring = require('querystring');
 
 const API_ENDPOINT = 'https://api.betterttv.net/2/';
 
+function request(method, path, options = {}) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: `${API_ENDPOINT}${path}${options.qs ? `?${querystring.stringify(options.qs)}` : ''}`,
+            method,
+            dataType: 'json',
+            data: options.body ? JSON.stringify(options.body) : undefined,
+            timeout: 30000,
+            success: data => resolve(data),
+            error: ({status, responseJSON}) => reject({
+                status,
+                data: responseJSON
+            })
+        });
+    });
+}
+
 module.exports = {
     get(path, options) {
-        return $.get(`${API_ENDPOINT}${path}`, options);
+        return request('GET', path, options);
+    },
+
+    post(path, options) {
+        return request('POST', path, options);
+    },
+
+    put(path, options) {
+        return request('PUT', path, options);
+    },
+
+    patch(path, options) {
+        return request('PATCH', path, options);
+    },
+
+    delete(path, options) {
+        return request('DELETE', path, options);
     }
 };
