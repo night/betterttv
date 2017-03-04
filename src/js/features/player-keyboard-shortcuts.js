3d05db0fd9eff0a8c08b26ed9bbda8fe3520af27
Ehsan Kia <contact@ehsankia.com>
2017-03-04 12:58:44 -0800

    Fix accidental triggering of player shortcuts

--- src/js/features/player-keyboard-shortcuts.js
+++ src/js/features/player-keyboard-shortcuts.js
@@ -2,6 +2,7 @@ var keyCodes = require('../keycodes');
 
 module.exports = function() {
     function handleKeyEvent(keyup) {
+        if (keyup.ctrlKey || keyup.metaKey || keyup.shiftKey) return;
         if (!$('input, textarea, select').is(':focus')) {
             if (keyup.keyCode === keyCodes.k) {
                 $('#player').find('.js-control-playpause-button').click();
