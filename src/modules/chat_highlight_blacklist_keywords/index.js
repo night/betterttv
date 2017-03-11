1fb32df6b9273c342d24ee9649ebefbd2169e0ab
Kirill Voloshin <voloshin.k@husky.neu.edu>
2017-03-10 23:20:56 -0500

    Pinned Highlights and Pinned Highlights Timeout (#2118)
    * Pinned Highlights and Pinned Highlights Timeout
    
    * Don't touch dom for historical message checks
    
    * If a message is blacklisted, return without checking for highlight

--- src/modules/chat_highlight_blacklist_keywords/index.js
+++ src/modules/chat_highlight_blacklist_keywords/index.js
@@ -1,5 +1,7 @@
+const $ = require('jquery');
 const watcher = require('../../watcher');
 const settings = require('../../settings');
+const html = require('../../utils/html');
 const twitch = require('../../utils/twitch');
 const {escape: escapeRegExp} = require('../../utils/regex');
 
@@ -13,6 +15,25 @@ Use spaces in the field to specify multiple keywords. Place {} around a set of w
 const HIGHLIGHT_KEYWORD_PROMPT = `Type some highlight keywords. Messages containing keywords will turn red to get your attention.
         
 Use spaces in the field to specify multiple keywords. Place {} around a set of words to form a phrase, <> inside the {} to use exact search, and () around a single word to specify a username. Wildcards (*) are supported.`;
+
+const CHAT_ROOM_SELECTOR = '.ember-chat .chat-room';
+const PINNED_HIGHLIGHT_ID = 'bttv-pinned-highlight';
+const PINNED_CONTAINER_ID = 'bttv-pin-container';
+const MAXIMUM_PIN_COUNT = 10;
+const PINNED_HIGHLIGHT_TIMEOUT = 60 * 1000;
+
+const pinnedHighlightTemplate = ({timestamp, from, message}) => `
+    <div id="${PINNED_HIGHLIGHT_ID}">
+        <span class="close">
+            <svg class="svg-close" height="8px" version="1.1" viewBox="0 0 16 16" width="8px" x="0px" y="0px">
+                <path clip-rule="evenodd" d="M13.657,3.757L9.414,8l4.243,4.242l-1.415,1.415L8,9.414l-4.243,4.243l-1.414-1.415L6.586,8L2.343,3.757l1.414-1.414L8,6.586l4.242-4.243L13.657,3.757z" fill-rule="evenodd"></path>
+            </svg>
+        </span>
+        <span class="time">${timestamp}</span>
+        <span class="display-name">${html.escape(from)}</span>
+        <span class="message">${html.escape(message)}</span>
+    </div>
+`;
 
 function changeKeywords(promptBody, settingID) {
     let keywords = prompt(promptBody, settings.get(settingID) || '');
@@ -109,6 +130,8 @@ function messageContainsKeyword(keywords, from, message) {
     return false;
 }
 
+let $pinnedHighlightsContainer;
+
 class ChatHighlightBlacklistKeywordsModule {
     constructor() {
         watcher.on('load.chat', () => {
@@ -118,6 +141,23 @@ class ChatHighlightBlacklistKeywordsModule {
         watcher.on('chat.message', ($message, messageObj) => this.onMessage($message, messageObj));
         settings.on('changed.blacklistKeywords', computeBlacklistKeywords);
         settings.on('changed.highlightKeywords', computeHighlightKeywords);
+
+        settings.add({
+            id: 'pinnedHighlights',
+            name: 'Pin Highlighted Messages',
+            defaultValue: false,
+            description: 'Pin your ten latest highlighted messages right above chat'
+        });
+        settings.on('changed.pinnedHighlights', value => value === true ? this.loadPinnedHighlits() : this.unloadPinnedHighlits());
+
+        settings.add({
+            id: 'timeoutHighlights',
+            name: 'Timeout Pinned Highlights',
+            defaultValue: false,
+            description: 'Automatically hide pinned highlights after 1 minute'
+        });
+
+        this.loadPinnedHighlits();
     }
 
     setBlacklistKeywords() {
@@ -128,13 +168,14 @@ class ChatHighlightBlacklistKeywordsModule {
         changeKeywords(HIGHLIGHT_KEYWORD_PROMPT, 'highlightKeywords');
     }
 
-    onMessage($message, {from, message}) {
+    onMessage($message, {from, message, date, tags}) {
         if (fromContainsKeyword(blacklistUsers, from) || messageContainsKeyword(blacklistKeywords, from, message)) {
-            this.markBlacklisted($message);
+            return this.markBlacklisted($message);
         }
 
         if (fromContainsKeyword(highlightUsers, from) || messageContainsKeyword(highlightKeywords, from, message)) {
             this.markHighlighted($message);
+            if (tags && !tags.historical) this.pinHighlight({from, message, date});
         }
     }
 
@@ -144,6 +185,40 @@ class ChatHighlightBlacklistKeywordsModule {
 
     markBlacklisted($message) {
         $message.hide();
+    }
+
+    loadPinnedHighlits() {
+        if (settings.get('pinnedHighlights') === false) return;
+
+        if (!$pinnedHighlightsContainer) {
+            $pinnedHighlightsContainer = $(`<div id=${PINNED_CONTAINER_ID}>`).appendTo($(CHAT_ROOM_SELECTOR));
+        }
+    }
+
+    unloadPinnedHighlits() {
+        if (!$pinnedHighlightsContainer) return;
+
+        $pinnedHighlightsContainer.remove();
+    }
+
+    pinHighlight({from, message, date}) {
+        if (settings.get('pinnedHighlights') === false) return;
+
+        if ($pinnedHighlightsContainer.children().length + 1 > MAXIMUM_PIN_COUNT) {
+            $pinnedHighlightsContainer.children().first().remove();
+        }
+        // probably not the best way to get a timestamp
+        const timestamp = `${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}`;
+
+        const $newHighlight = $(pinnedHighlightTemplate({timestamp, from, message}));
+
+        $newHighlight.children('.close').on('click', () => $newHighlight.remove());
+
+        $pinnedHighlightsContainer.append($newHighlight);
+
+        if (settings.get('timeoutHighlights') === true) {
+            setTimeout(() => $newHighlight.remove(), PINNED_HIGHLIGHT_TIMEOUT);
+        }
     }
 }
 
