35b00939befc62fc4c55a3fbefaafe755f9dbc39
Thomas Buida <thomas@gamewisp.com>
2017-03-16 14:55:13 -0500

    data model refactor.

--- src/modules/gamewisp/subscriber-emotes.js
+++ src/modules/gamewisp/subscriber-emotes.js
@@ -30,6 +30,8 @@ class SubscriberEmotes extends AbstractEmotes {
     constructor() {
         super();
 
+        this.emoteUsers = new Map();
+
         socketClient.on('initialize_room', data => this.initializeRoom(data));
         socketClient.on('update_room', data => this.updateRoom(data));
         socketClient.on('leave_room', data => this.leaveRoom(data));
@@ -48,19 +50,35 @@ class SubscriberEmotes extends AbstractEmotes {
     getEmotes(user) {
         if (!user) return [];
 
-        const emotes = this.emotes.get(user.name);
-        if (!emotes) return [];
+        const emoteIDs = this.emoteUsers.get(user.name);
+        if (!emoteIDs) return [];
 
-        return [...emotes.values()];
+        const emotes = [];
+
+        emoteIDs.forEach(id => {
+            if (this.emotes.has(id)) {
+                emotes.push(this.emotes.get(id));
+            }
+        });
+
+        return emotes;
     }
 
     getEligibleEmote(code, user) {
-        if (!user) return;
+        if (!user || !this.emoteUsers) return;
 
-        const emotes = this.emotes.get(user.name);
-        if (!emotes) return;
+        const emoteIDs = this.emoteUsers.get(user.name);
+        if (!emoteIDs) return;
 
-        return emotes.get(code);
+        let emote;
+        for (const id of emoteIDs) {
+            emote = this.emotes.get(id);
+            if (emote && emote.code === code) {
+                return emote;
+            }
+        }
+
+        return;
     }
 
     joinRoom() {
@@ -71,117 +89,136 @@ class SubscriberEmotes extends AbstractEmotes {
 
     initializeRoom({userStore, emotes}) {
         emotes = emotes.map(emoteData => buildEmote(emoteData)) || [];
+        emotes.forEach(emote => this.emotes.set(emote.id, emote));
 
         for (const username in userStore) {
             if (userStore.hasOwnProperty(username)) {
-                let userEmotes = this.emotes.get(name);
-                if (!userEmotes) {
-                    userEmotes = new Map();
-                    this.emotes.set(username, userEmotes);
+                let userEmoteIDs = this.emotes.get(name);
+                if (!userEmoteIDs) {
+                    userEmoteIDs = [];
                 }
 
-                const emoteIDsSet = new Set(userStore[username]);
+                userEmoteIDs = [...userEmoteIDs, ...userStore[username]];
 
-                emotes.forEach(e => {
-                    if (emoteIDsSet.has(e.id)) {
-                        userEmotes.set(e.code, e);
-                    }
-                });
+                this.emoteUsers.set(username, userEmoteIDs);
             }
         }
 
         console.log('this.emotes', this.emotes);
+        console.log('this.emoteUsers', this.emoteUsers);
     }
 
     updateRoom({emotes, user}) {
         let newEmotes = emotes || [];
 
         newEmotes = newEmotes.map(emoteData => buildEmote(emoteData));
+        newEmotes.forEach(emote => this.emotes.set(emote.id, emote));
 
-        let userEmotes = this.emotes.get(user.name);
-        if (!userEmotes) {
-            userEmotes = new Map();
-            this.emotes.set(user.name, userEmotes);
+        let userEmoteIDs = this.emoteUsers.get(user.name);
+        if (!userEmoteIDs) {
+            userEmoteIDs = [];
         }
 
-        const userEmoteIDsSet = new Set(user.emoteIDs);
+        userEmoteIDs = [...userEmoteIDs, ...user.emoteIDs];
 
-        newEmotes.forEach(e => {
-            if (userEmoteIDsSet.has(e.id)) {
-                userEmotes.set(e.code, e);
-            }
-        });
+        this.emoteUsers.set(user.name, userEmoteIDs);
 
         console.log('this.emotes', this.emotes);
+        console.log('this.emoteUsers', this.emoteUsers);
     }
 
     leaveRoom({user}) {
-        if (this.emotes.get(user)) {
-            this.emotes.delete(user);
+        if (this.emoteUsers.get(user)) {
+            this.emoteUsers.delete(user);
         }
         console.log('this.emotes', this.emotes);
+        console.log('this.emoteUsers', this.emoteUsers);
     }
 
-    removeEmote({emoteCode}) {
-        const emotes = [...this.emotes.values()];
+    removeEmote({emoteID}) {
+        console.log('emoteID', emoteID);
 
-        emotes.forEach(emoteMap => {
-            emoteMap.delete(emoteCode);
+        const emoteUsers = [...this.emoteUsers.values()];
+
+        let idx;
+        emoteUsers.forEach(emoteIDs => {
+            idx = emoteIDs.indexOf(emoteID);
+
+            if (idx >= 0) {
+                emoteIDs.splice(idx, 1);
+            }
         });
 
+        if (this.emotes.has(emoteID)) {
+            this.emotes.delete(emoteID);
+        }
+
         console.log('this.emotes', this.emotes);
+        console.log('this.emoteUsers', this.emoteUsers);
     }
 
     addEmote({emote, emoteUsers}) {
         emote = buildEmote(emote);
 
-        let userEmotes;
+        let userEmoteIDs;
 
         emoteUsers.forEach(user => {
-            if (this.emotes.has(user)) {
-                userEmotes = this.emotes.get(user);
+            if (this.emoteUsers.has(user)) {
+                userEmoteIDs = this.emoteUsers.get(user);
             } else {
-                userEmotes = new Map();
-                this.emotes.set(user, userEmotes);
+                userEmoteIDs = [];
             }
 
-            userEmotes.set(emote.code, emote);
+            userEmoteIDs = [...userEmoteIDs, emote.id];
+
+            this.emoteUsers.set(user, userEmoteIDs);
+            this.emotes.set(emote.id, emote);
         });
 
         console.log('this.emotes', this.emotes);
+        console.log('this.emoteUsers', this.emoteUsers);
     }
 
-    newSubscriber({user, emotes}) {
-        const newSub = user;
-        let userEmotes;
-
-        if (this.emotes.has(newSub)) {
-            userEmotes = this.emotes.get(newSub);
+    newSubscriber({user, emotes, emoteIDs}) {
+        let userEmoteIDs;
+        if (this.emoteUsers.has(user)) {
+            userEmoteIDs = this.emoteUsers.get(user);
         } else {
-            userEmotes = new Map();
-            this.emotes.set(newSub, userEmotes);
+            userEmoteIDs = [];
         }
 
-        emotes.forEach(gwEmote => {
-            userEmotes.set(gwEmote.code, buildEmote(gwEmote));
+        userEmoteIDs = [...userEmoteIDs, ...emoteIDs];
+        this.emoteUsers.set(user, userEmoteIDs);
+
+        emotes.forEach(emote => {
+            if (!this.emotes.has(emote.id)) {
+                this.emotes.set(emote.id, buildEmote(emote));
+            }
         });
 
         console.log('this.emotes', this.emotes);
+        console.log('this.emoteUsers', this.emoteUsers);
     }
 
     cancelSubscriber({user, emoteIDs}) {
-        if (!this.emotes.has(user)) return;
+        console.log('cancelSubscriber', emoteIDs);
+        if (!this.emoteUsers.has(user)) return;
 
-        const emoteIDsToRemove = new Set(emoteIDs);
-        const userEmotes = this.emotes.get(user);
+        const userEmoteIDs = this.emoteUsers.get(user);
 
-        userEmotes.forEach(emote => {
-            if (emoteIDsToRemove.has(emote.id)) {
-                userEmotes.delete(emote.code);
+        let idx;
+        emoteIDs.forEach(idToRemove => {
+            idx = userEmoteIDs.indexOf(idToRemove);
+
+            if (idx >= 0) {
+                userEmoteIDs.splice(idx, 1);
             }
         });
 
+        this.emoteUsers.set(user, userEmoteIDs);
+
         console.log('this.emotes', this.emotes);
+        console.log('this.emoteUsers', this.emoteUsers);
     }
 }
 
