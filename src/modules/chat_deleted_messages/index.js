f1e236ae6f9434e199e22632b1b894957897fec0
Night <night@nightdev.com>
2017-03-19 18:26:03 -0700

    fixes #2142

--- src/modules/chat_deleted_messages/index.js
+++ src/modules/chat_deleted_messages/index.js
@@ -7,13 +7,13 @@ class ChatDeletedMessagesModule {
             id: 'showDeletedMessages',
             name: 'Show Deleted Messages',
             defaultValue: false,
-            description: 'Completely removes timed out messages from view'
+            description: 'Turn this on to change <message deleted> back to users\' messages.'
         });
         settings.add({
             id: 'hideDeletedMessages',
             name: 'Remove Deleted Messages',
             defaultValue: false,
-            description: 'Turn this on to change <message deleted> back to users\' messages.'
+            description: 'Completely removes timed out messages from view'
         });
         watcher.on('chat.message.deleted', ($element, emberView) => this.onMessage($element, emberView));
     }
