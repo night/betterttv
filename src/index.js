654c22d69d36c97b1174b5893da9312b85ba8b6d
Night <night@nightdev.com>
2017-03-20 23:34:52 -0700

    bugfix babel polyfill already loaded

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 (() => {
-    require('babel-polyfill');
+    if (!window._babelPolyfill) {
+        require('babel-polyfill');
+    }
 
     if (window.location.pathname.endsWith('.html')) return;
 
@@ -11,25 +13,9 @@
             {
                 release: process.env.GIT_REV,
                 environment: process.env.NODE_ENV,
-                ignoreErrors: [
-                    'Blocked a frame with origin',
-                    'player-core-min',
-                    'NS_ERROR_NOT_INITIALIZED',
-                    'InvalidAccessError',
-                    /^<anonymous> in/
-                ],
-                ignoreUrls: [
-                    '/script/script.min.js',
-                    '/js/sdkloader/ima3.js',
-                    '/js/sdkloader/loader.js',
-                    'player.js',
-                    'player-core-min.js',
-                    'instream/video/client.js',
-                    'cdn.flashtalking.com',
-                    /emberapp\-[a-z0-9]+\.js/,
-                    /global\-[a-z0-9]+\.js/,
-                    /tmi\-v3\.[a-z0-9]+\.js/,
-                    /emberhelper\-[a-z0-9]+\.js/
+                whitelistUrls: [
+                    /betterttv\.js/,
+                    /\.betterttv\.net/
                 ]
             }
         ).install();
