18fa7655ef7224fd45f858b318f444c815b4430b
Ehsan Kia <contact@ehsankia.com>
2017-03-22 01:37:58 -0700

    Cleaning up anon chat api

--- src/modules/anon_chat/index.js
+++ src/modules/anon_chat/index.js
@@ -14,44 +14,43 @@ class AnonChatModule {
             description: 'Join channels without appearing in chat'
         });
 
+        this.enabled = false;
         watcher.on('load.chat', () => this.load());
         watcher.on('chat.message', ($el, emberView) => this.onMessage($el, emberView));
         settings.on('changed.anonChat', () => this.load());
     }
 
-    part() {
-        const currentUser = twitch.getCurrentUser();
+    changeUser(username, message) {
         const tmiSession = twitch.getCurrentTMISession();
-        if (!currentUser || !tmiSession) return;
+        if (!tmiSession) return;
 
         const prodConn = tmiSession._connections.main;
-        if (prodConn._opts.nickname !== currentUser.name) return;
+        if (prodConn._opts.nickname === username) return;
 
-        prodConnOpts.nickname = 'justinfan12345';
-        twitch.sendChatAdminMessage('BetterTTV: [Anon Chat] Logging you out of chat...');
         ignoreNextDC = true;
+        prodConn._opts.nickname = username;
+        twitch.sendChatAdminMessage(`BetterTTV: [Anon Chat] ${message}`);
         prodConn._send('QUIT');
+    }
+
+    part() {
+        this.changeUser('justinfan12345', 'Logging you out of chat...');
+        this.enabled = true;
     }
 
     join() {
         const currentUser = twitch.getCurrentUser();
-        const tmiSession = twitch.getCurrentTMISession();
-        if (!currentUser || !tmiSession) return;
+        if (!currentUser) return;
 
-        const prodConn = tmiSession._connections.main;
-        if (prodConn._opts.nickname === currentUser.name) return;
-
-        prodConnOpts.nickname = currentUser.name;
-        twitch.sendChatAdminMessage('BetterTTV: [Anon Chat] Logging you into chat...');
-        ignoreNextDC = true;
-        prodConn._send('QUIT');
+        this.changeUser(currentUser.name, 'Logging you into chat...');
+        this.enabled = false;
     }
 
     load() {
         if (forcedURL || settings.get('anonChat')) {
-            this.join();
-        } else {
             this.part();
+        } else {
+            this.join();
         }
     }
 
