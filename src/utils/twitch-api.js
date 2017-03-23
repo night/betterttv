48a7499f9a5df12b77e9611f3559997fa73f524a
Night <night@nightdev.com>
2017-03-23 00:32:09 -0700

    bugfix

--- src/utils/twitch-api.js
+++ src/utils/twitch-api.js
@@ -8,15 +8,29 @@ const CLIENT_ID = '6x8avioex0zt85ht6py4sq55z6avsea';
 function request(method, path, options = {}) {
     return new Promise((resolve, reject) => {
         const currentUser = twitch.getCurrentUser();
+        const fullPath = `${path}${options.qs ? `?${querystring.stringify(options.qs)}` : ''}`;
+
+        // temp fix until twitch fixes cors
+        if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(method)) {
+            method = method.toLowerCase().replace('delete', 'del');
+            window.Twitch
+                .api[method](fullPath, options.body, {
+                    headers: {Authorization: `OAuth ${currentUser.accessToken}`},
+                    version: 5
+                })
+                .done(data => resolve(data))
+                .fail(() => reject());
+            return;
+        }
 
         $.ajax({
-            url: `${API_ENDPOINT}${path}${options.qs ? `?${querystring.stringify(options.qs)}` : ''}`,
+            url: `${API_ENDPOINT}${fullPath}`,
             method,
             dataType: 'json',
             data: options.body ? JSON.stringify(options.body) : undefined,
             headers: {
                 'Client-ID': CLIENT_ID,
-                'Authorization': (options.auth && currentUser) ? currentUser.accessToken : undefined
+                'Authorization': (options.auth && currentUser) ? `OAuth ${currentUser.accessToken}` : undefined
             },
             xhrFields: {
                 withCredentials: options.auth
