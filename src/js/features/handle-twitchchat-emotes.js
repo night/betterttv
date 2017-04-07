186655a76e2894938f6daf9cba225455707c65c0
night <night@nightdev.com>
2017-04-07 02:47:44 -0500

    hackfix to try and get emote menu to work

--- src/js/features/handle-twitchchat-emotes.js
+++ src/js/features/handle-twitchchat-emotes.js
@@ -14,24 +14,28 @@ module.exports = function() {
     // Inject the emote menu if option is enabled.
     if (bttv.settings.get('clickTwitchEmotes') === false) return;
 
-    debug.log('Injecting Twitch Chat Emotes Script');
-
-    // require('twitch-chat-emotes/script.min');
-
     // Try hooking into the emote menu, regardless of whether we injected or not.
-    // var counter = 0;
-    // var getterInterval = setInterval(function() {
-    //     counter++;
+    var counter = 0;
+    var loaded = false;
+    var getterInterval = setInterval(function() {
+        counter++;
 
-    //     if (counter > 29) {
-    //         clearInterval(getterInterval);
-    //         return;
-    //     }
+        if (!loaded && !window.emoteMenu) {
+            loaded = true;
+            debug.log('Injecting Twitch Chat Emotes Script');
+            require('twitch-chat-emotes/script.min');
+            return;
+        }
 
-    //     if (window.emoteMenu) {
-    //         clearInterval(getterInterval);
-    //         debug.log('Hooking into Twitch Chat Emotes Script');
-    //         window.emoteMenu.registerEmoteGetter('BetterTTV', bttv.chat.emotes);
-    //     }
-    // }, 1000);
+        if (counter > 29) {
+            clearInterval(getterInterval);
+            return;
+        }
+
+        if (window.emoteMenu) {
+            clearInterval(getterInterval);
+            debug.log('Hooking into Twitch Chat Emotes Script');
+            window.emoteMenu.registerEmoteGetter('BetterTTV', bttv.chat.emotes);
+        }
+    }, 5000);
 };
