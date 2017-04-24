4b6456a108a2f47393596e67ad04dc00df8b68b9
Night <night@nightdev.com>
2017-04-24 00:31:32 -0700

    ctrl to pause now toggles

--- src/modules/chat_freeze/index.js
+++ src/modules/chat_freeze/index.js
@@ -5,8 +5,7 @@ const watcher = require('../../watcher');
 const CHAT_CONTAINER = '.chat-room .chat-messages';
 const CHAT_LINES = '.ember-chat .chat-messages .chat-lines';
 const CHAT_ROOM_SELECTOR = '.chat-room > div.ember-view';
-
-let isFrozen = false;
+const MESSAGES_INDICATOR_SELECTOR = '.more-messages-indicator';
 
 function setScrollState(enabled) {
     const chatRoomEmberId = $(CHAT_ROOM_SELECTOR).attr('id');
@@ -25,20 +24,15 @@ class ChatFreezeModule {
         $('body')
             .on('keydown.chat-freeze', e => {
                 if (!(e.metaKey || e.ctrlKey) || !$(`${CHAT_LINES}:hover`).length || document.hidden) return;
-                isFrozen = true;
-                setScrollState(false);
-            })
-            .on('keyup.chat-freeze', e => {
-                if (e.metaKey || e.ctrlKey || !isFrozen) return;
-                isFrozen = false;
-                setScrollState(true);
+                const indicator = $(MESSAGES_INDICATOR_SELECTOR).length > 0;
+                setScrollState(indicator);
             });
         watcher.on('load.chat', () => this.load());
     }
 
     load() {
         $(CHAT_CONTAINER).on('mousewheel', ({originalEvent}) => {
-            const indicator = $('.more-messages-indicator').length > 0;
+            const indicator = $(MESSAGES_INDICATOR_SELECTOR).length > 0;
             if (originalEvent.wheelDelta > 0 && !indicator) {
                 setScrollState(false);
             } else if (originalEvent.wheelDelta < 0 && indicator) {
