214f81f61daa185c781607eee3944600e72fcca4
Nico Zimmermann <nicotom.zim@gmail.com>
2017-08-10 17:50:55 +0200

    Comments

--- src/modules/global_ban_protection/index.js
+++ src/modules/global_ban_protection/index.js
@@ -1,6 +1,7 @@
 const twitch = require('../../utils/twitch');
 
-// Regular users are allowed 20 messages in 30 seconds, mods (and higher) 100 messages in 30 seconds, see
+// Regular users are allowed 20 messages in 30 seconds, mods (and higher
+// positions) 100 messages in 30 seconds, see
 // https://discuss.dev.twitch.tv/t/message-limit-for-moderators/3042/4
 const regularMsgLimit = 20;
 const modMsgLimit = 100;
@@ -18,11 +19,12 @@ class GlobalBanProtectionModule {
     }
 
     onSendMessage(sendState) {
+        // Clear collected time stamps older than timeLimit seconds as they are
+        // no longer relevant for a global ban.
         while (true) {
             if (this.msgTimeStamps.length === 0) {
                 break;
             }
-
             const oldestMsgTimeStamp = this.msgTimeStamps.shift();
             if ((Date.now() - oldestMsgTimeStamp) / 1000 <= timeLimit) {
                 this.msgTimeStamps.unshift(oldestMsgTimeStamp);
@@ -30,6 +32,9 @@ class GlobalBanProtectionModule {
             }
         }
 
+        // At this point, this.msgTimeStamps.length is the number of messages
+        // sent in the last timeLimit seconds. If there is room for another
+        // message, collect its time stamp. Else, prevent the global ban.
         if (this.msgTimeStamps.length < this.msgLimit) {
             this.msgTimeStamps.push(Date.now());
         } else {
