5e2f7531e0f4fbbcbec9356572988ccaf22efa73
Rhino <rhino@notmyrealemail.com>
2017-08-27 06:05:37 -0700

    Add two new options...
    Adds an option to hide offline followed channels. This will allow people
    who follow a lot of streamers and need to always use the "Load More"
    option to not have a scroll bar on their side bar. With this enabled,
    following 100+ people is less of a chore.
    
    Also adds an option to hide Recommended Friends. There was a bug in
    `betterttv-hide-friends.css` causing it not no longer be removed but I
    prefer having a seperate option as I heavily moderate who I add as a
    friend making that section useless for me.
    
    The former option is more useful than the latter which arose from a
    simple bug so feel free to rip one and not the other.

--- src/modules/hide_sidebar_elements/index.js
+++ src/modules/hide_sidebar_elements/index.js
@@ -12,17 +12,48 @@ class HideSidebarElementsModule {
             description: 'The left sidebar is too cluttered, so you can remove recommended channels'
         });
         settings.add({
+            id: 'hideRecommendedFriends',
+            name: 'Hide Recommended Friends',
+            defaultValue: true,
+            description: 'Hides the Recommended Friends section so you have more room for activities!'
+        });
+        settings.add({
+            id: 'hideOfflineFollowedChannels',
+            name: 'Hide Offline Followed Channels',
+            defaultValue: false,
+            description: 'Hides all offline followed channels for those who follow a ton of channels!'
+        });
+        settings.add({
             id: 'hidePrimePromotion',
             name: 'Hide Prime Promotions',
             defaultValue: false,
             description: 'Hides the "Free With Prime" section of the sidebar'
         });
         settings.on('changed.hideFeaturedChannels', () => this.toggleFeaturedChannels());
+        settings.on('changed.hideRecommendedFriends', () => this.toggleRecommendedFriend());
+        settings.on('changed.hideOfflineFollowedChannels', () => this.toggleOfflineFollowedChannels());
         settings.on('changed.hidePrimePromotion', () => this.togglePrimePromotions());
         watcher.on('load', () => {
             this.toggleFeaturedChannels();
+            this.toggleRecommendedFriend();
             this.togglePrimePromotions();
+            this.toggleOfflineFollowedChannels();
         });
+    }
+
+    toggleOfflineFollowedChannels() {
+        if (settings.get('hideOfflineFollowedChannels') === false) {
+            css.unload('hide-offline-channels');
+        } else {
+            css.load('hide-offline-channels');
+        }
+    }
+    toggleRecommendedFriend() {
+        if (settings.get('hideRecommendedFriends') === false) {
+            css.unload('hide-recommended-friends');
+        } else {
+            css.load('hide-recommended-friends');
+        }
     }
 
     toggleFeaturedChannels() {
