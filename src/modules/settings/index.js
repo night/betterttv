28ea62d83cf1a760cd35c735565270a2e7d8f302
Night <night@nightdev.com>
2017-10-07 13:10:54 -0700

    add settings warning

--- src/modules/settings/index.js
+++ src/modules/settings/index.js
@@ -39,7 +39,8 @@ const settingsPanelTemplate = () => `
         <span id="close">&times;</span>
     </div>
     <div id="bttvSettings" class="options-list">
-        <h2 class="option">Here you can manage the various BetterTTV options. Click On or Off to toggle settings.</h2>
+        <h2 class="option" style="color: orange;">🚨 🚧 ⚠️ BetterTTV for Twitch's Beta site is incomplete. We know things are broken/missing. ⚠️ 🚧 🚨</h2>
+        <!--h2 class="option">Here you can manage the various BetterTTV options. Click On or Off to toggle settings.</h2-->
     </div>
     <div id="bttvAbout" style="display:none;">
         <div class="aboutHalf">
