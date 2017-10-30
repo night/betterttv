aeee7ac619937310bbd9a4bdfe2f7ad9e58a59b2
Aaron L <aloken128@gmail.com>
2017-10-29 19:51:57 -0600

    Revert "Remove the ability to toggle Twitch's Dark Mode from BetterTTV"
    This reverts commit 68df1e753160ceb27fbcb2fd9806d3bcf46ef618.

--- src/modules/global_css/index.js
+++ src/modules/global_css/index.js
@@ -1,12 +1,41 @@
 const $ = require('jquery');
 const cdn = require('../../utils/cdn');
+const settings = require('../../settings');
 const watcher = require('../../watcher');
+const twitch = require('../../utils/twitch');
+
+const CHAT_SETTINGS_DARK_TOGGLE_SELECTOR = '#chat-settings-dark-mode';
+
+function toggleDarkChat(value) {
+    const chatDarkModeToggle = twitch.getReactElement($(CHAT_SETTINGS_DARK_TOGGLE_SELECTOR)[0]);
+    if (!chatDarkModeToggle) return;
+    const {checked, onChange} = chatDarkModeToggle.props;
+    if ((!checked && !value) || (checked && value)) return;
+    onChange();
+}
 
 class GlobalCSSModule {
     constructor() {
         this.globalCSS();
+
         watcher.on('load', () => this.branding());
+        watcher.on('load.chat', () => this.loadDark());
         this.branding();
+
+        settings.add({
+            id: 'darkenedMode',
+            name: 'Dark Theme',
+            defaultValue: false,
+            description: 'Enable Twitch\'s dark theme'
+        });
+        settings.on('changed.darkenedMode', () => this.loadDark());
+        this.loadDark();
+    }
+
+    loadDark() {
+        const value = settings.get('darkenedMode');
+        $('html').toggleClass('theme--dark', value);
+        toggleDarkChat(value);
     }
 
     globalCSS() {
