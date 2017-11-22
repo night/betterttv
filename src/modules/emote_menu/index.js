25365f155dd50c96a6cab4f4794c6748d1114643
hobbycoder7 <hobbycoder7@users.noreply.github.com>
2017-11-22 16:16:12 -0600

    Added callback to emote menu load

--- src/modules/emote_menu/index.js
+++ src/modules/emote_menu/index.js
@@ -33,18 +33,23 @@ class EmoteMenuModule {
         require('twitch-chat-emotes/script.min');
 
         debug.log('Hooking into Twitch Chat Emotes Script');
-        try { // try/catch protects against re-registered emote getters
-            window.emoteMenu.registerEmoteGetter('BetterTTV', () =>
-                emotes.getEmotes(['bttv-emoji']).map(({code, images, provider}) => {
-                    return {
-                        text: code,
-                        channel: provider.displayName,
-                        badge: provider.badge,
-                        url: images['2x']
-                    };
-                })
-            );
-        } catch (e) {}
+        this.loadEmotes();
+    }
+    loadEmotes() {
+        window.emoteMenu.onLoad(() => {
+            try { // try/catch protects against re-registered emote getters
+                window.emoteMenu.registerEmoteGetter('BetterTTV', () =>
+                    emotes.getEmotes(['bttv-emoji']).map(({code, images, provider}) => {
+                        return {
+                            text: code,
+                            channel: provider.displayName,
+                            badge: provider.badge,
+                            url: images['2x']
+                        };
+                    })
+                );
+            } catch (e) {}
+        });
     }
 }
 
