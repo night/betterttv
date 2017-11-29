b3182706df3d742ff44eea816b92dee2f09dc127
hobbycoder7 <hobbycoder7@users.noreply.github.com>
2017-11-29 15:09:10 -0600

    Requested Fixes
    - Proper jquery require
    - Selector moved to const
    - Attempt at better description

--- src/modules/hide_extensions/index.js
+++ src/modules/hide_extensions/index.js
@@ -1,6 +1,8 @@
-const $ = require('JQuery');
+const $ = require('jquery');
 const settings = require('../../settings');
 const watcher = require('../../watcher');
+
+const EXTENSION_CONTAINER = '#js-player-extension-root';
 
 class HideExtensionsModule {
     constructor() {
@@ -8,14 +10,14 @@ class HideExtensionsModule {
             id: 'hideExtensions',
             name: 'Hide Extensions',
             defaultValue: false,
-            description: 'Hide Twitch Extensions'
+            description: 'Hides Twitch extensions that are displayed over the video player'
         });
         watcher.on('load.player', () => this.load());
     }
 
     load() {
         if (settings.get('hideExtensions') === false) return;
-        $('#js-player-extension-root').hide();
+        $(EXTENSION_CONTAINER).hide();
     }
 }
 
