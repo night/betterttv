14a32ed4e9983e69cc0dcad43bd3b4b8cf2dcbd2
ben <benj.groeneveld@gmail.com>
2017-12-07 01:05:37 +0100

    changed emote loading

--- src/modules/chat_emotes_suggestion/index.js
+++ src/modules/chat_emotes_suggestion/index.js
@@ -1,12 +1,8 @@
-// const $ = require('jquery');
 const emotes = require('../emotes');
 const twitch = require('../../utils/twitch');
-// const watcher = require('../../watcher');
-// const debounce = require('lodash.debounce');
+const watcher = require('../../watcher');
 
 const BTTV_EMOTES_STORE_ID = 'bttv-emotes';
-// const BTTV_EMOTES_STORE_ID = '1';
-// const CHAT_INPUT = '.chat-input';
 
 function esca(str) {
     return str.replace(/\(/g, '\\(')
@@ -24,58 +20,25 @@ function bttvEmToTwitch(bttvEm) {
     };
 }
 
-window.emotes = emotes;
-window.twitch = twitch;
-
 function getBttvEmotesAsTwitchEmotes(emoteGetter) {
-    // const idSet = new Set();
-    // const codeSet = new Set();
     return emoteGetter()
-        // .filter(em => isASCII(em.code))
-        // .filter(em => {
-        //     if (!idSet.has('' + em.id) || !codeSet.has(em.code)) {
-        //         idSet.add('' + em.id);
-        //         codeSet.add(em.code);
-        //         return true;
-        //     }
-        //     return false;
-        // })
-        // .filter(em => !em.code.startsWith(':')) // emojis or any emote using special char break the chat.
         .map(bttvEmToTwitch);
 }
 
 class ChatEmotesSuggestion {
     constructor() {
-        // const debounceLoadEmotes = debounce(() => this.loadEmotes(), 500, { leading: true, trailing: false });
-        // $('body').on('keydown', CHAT_INPUT, () => {
-        //     debounceLoadEmotes();
-        // });
-        window.AAA = this;
-        // let timeout;
-        // const checkForEmotes = () => {
-        //     clearTimeout(timeout);
-        //     const nb = this.applyNewStore();
-        //     console.log('UPT');
-        //     if (!nb) {
-        //         timeout = setTimeout(() => {
-        //             checkForEmotes();
-        //         }, 500);
-        //     }
-        // };
-        // watcher.on('emotes.updated', () => checkForEmotes());
-
-        // watcher.on('load.chat', () => {
-        //     const x = twitch.getChatController().updateEmoteSets;
-        //     twitch.getChatController().updateEmoteSets = function(e) {
-        //         console.log('EEE', e);
-        //         window.EEE = e;
-        //         return x.apply(this, arguments);
-        //     };
-        // });
-
-        setInterval(() => {
-            this.applyNewStore();
-        }, 500);
+        let intervalId;
+        watcher.on('emotes.updated', () => {
+            clearInterval(intervalId);
+            let retry = 15;
+            intervalId = setInterval(() => {
+                this.applyNewStore();
+                retry--;
+                if (retry <= 0) {
+                    clearInterval(intervalId);
+                }
+            }, 500);
+        });
     }
 
     validateEmoteStore() {
@@ -94,7 +57,7 @@ class ChatEmotesSuggestion {
     }
 
     emotes() {
-        return getBttvEmotesAsTwitchEmotes(() => emotes.getEmotes()); // ['bttv-emoji']
+        return getBttvEmotesAsTwitchEmotes(() => emotes.getEmotes());
     }
 
     cloneTwitchEmotes() {
@@ -123,13 +86,11 @@ class ChatEmotesSuggestion {
 
     applyNewStore() {
         if (!this.validateEmoteStore()) return;
+
         const [st, bttvEmotes] = this.createNewStore();
         twitch.getChatController().props.emoteSets = st;
-        console.log('store updated');
         return bttvEmotes.length;
-        // twitch.getConnectRoot()._context.store.getState().chat.emoteSets = st;
     }
 }
-
 
 module.exports = new ChatEmotesSuggestion();
