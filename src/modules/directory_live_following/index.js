588f770465f6007a093939b5f7ee3713eebbd091
ben <benj.groeneveld@gmail.com>
2017-12-11 19:07:50 +0100

    auto click expand

--- src/modules/directory_live_following/index.js
+++ src/modules/directory_live_following/index.js
@@ -10,12 +10,34 @@ class DirectoryLiveFollowingModule {
             defaultValue: false,
             description: 'BetterTTV can click on "Channels" for you in the Following Overview automatically'
         });
+        settings.add({
+            id: 'autoExpandChannels',
+            name: 'Auto Expand Followed Live Channels List',
+            defaultValue: false,
+            description: 'Automatically clicks the "Load More" option for you'
+        });
+
         watcher.on('load.directory.following', () => this.load());
+        watcher.on('load', () => this.toggleAutoExpandChannels());
     }
 
     load() {
         if (settings.get('showDirectoryLiveTab') === false) return;
         $('a[href="/directory/following/live"]')[0].click();
+    }
+
+    toggleAutoExpandChannels() {
+        if (!window.location.href.includes('directory/following/live')) return;
+        if (!settings.get('autoExpandChannels')) return;
+
+        $('.following__view-all').click();
+        const expandInterval = setInterval(() => {
+            $('.following__view-all').click();
+        }, 100);
+
+        setTimeout(() => {
+            clearInterval(expandInterval);
+        }, 3000);
     }
 }
 
