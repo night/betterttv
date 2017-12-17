73cb45f1f1704ee2462bccae55ffb9e13e4f8274
ben <benj.groeneveld@gmail.com>
2017-12-17 19:09:04 +0100

    remove original twitch function

--- src/modules/chat_moderator_cards/index.js
+++ src/modules/chat_moderator_cards/index.js
@@ -19,7 +19,8 @@ const CHAT_INPUT_SELECTOR = '.chat-input textarea';
 
 const BTTV_MOD_CARDS_ID = 'bttv-mod-cards';
 const BTTV_MOD_CARDS_SELECTOR = `#${BTTV_MOD_CARDS_ID}`;
-const BTTV_MOD_SECTION_ID = 'bttv-mod-section';
+const BTTV_MOD_SECTION_CLASS = 'bttv-mod-section';
+const BTTV_OWNER_SECTION_CLASS = 'bttv-owner-section';
 const BTTV_HIDE_SECTION_CLASS = 'bttv-hide-section';
 const BTTV_USER_MESSAGES_ID = 'bttv-user-messages';
 
@@ -29,20 +30,42 @@ const BTTV_ACTION_ATTR = 'bttv-action';
 const BTTV_ACTION_VAL_ATTR = 'bttv-val';
 
 const ACTIONS = {
+    BAN: 'BAN',
+    UNBAN: 'UNBAN',
+    MOD: 'MOD',
+    UNMOD: 'UNMOD',
     TIMEOUT: 'TIMEOUT',
     PERMIT: 'PERMIT',
     MESSAGES: 'MESSAGES'
 };
 
 const ACTIONS_MAP = {
+    BAN: '/ban',
+    UNBAN: '/unban',
+    MOD: '/mod',
+    UNMOD: '/unmod',
     TIMEOUT: '/timeout',
     PERMIT: '!permit'
 };
 
+const svgCheck = '<figure class="tw-svg"><svg class="tw-svg__asset tw-svg__asset--followcheck tw-svg__asset--inherit" width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px"><path clip-rule="evenodd" d="M6.5,12.75L2,8.25l2-2l2.5,2.5l5.5-5.5l2,2L6.5,12.75z" fill-rule="evenodd"></path></svg></figure>';
+const svgBlock = '<figure class="tw-svg"><svg class="tw-svg__asset tw-svg__asset--ban tw-svg__asset--inherit" width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px"><path clip-rule="evenodd" d="M8,15c-3.866,0-7-3.134-7-7s3.134-7,7-7s7,3.134,7,7S11.866,15,8,15z M3,8c0,1.019,0.309,1.964,0.832,2.754l6.922-6.922C9.964,3.309,9.019,3,8,3C5.238,3,3,5.238,3,8z M12.169,5.246l-6.923,6.923C6.036,12.691,6.98,13,8,13c2.762,0,5-2.238,5-5C13,6.98,12.691,6.036,12.169,5.246z" fill-rule="evenodd"></path></svg></figure>';
+const svgHeart = '<figure class="tw-svg"><svg class="tw-svg__asset tw-svg__asset--heart tw-svg__asset--inherit" width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px"><path clip-rule="evenodd" d="M8,14L1,7V4l2-2h3l2,2l2-2h3l2,2v3L8,14z" fill-rule="evenodd"></path></svg></figure>';
+const svgEye = '<figure class="tw-svg"><svg class="tw-svg__asset tw-svg__asset--glyphviews tw-svg__asset--inherit" width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px"><path clip-rule="evenodd" d="M11,13H5L1,9V8V7l4-4h6l4,4v1v1L11,13z M8,5C6.344,5,5,6.343,5,8c0,1.656,1.344,3,3,3c1.657,0,3-1.344,3-3C11,6.343,9.657,5,8,5z M8,9C7.447,9,7,8.552,7,8s0.447-1,1-1s1,0.448,1,1S8.553,9,8,9z" fill-rule="evenodd"></path></svg></figure>';
+const svgPen = '<figure class="tw-svg"><svg class="tw-svg__asset tw-svg__asset--edit tw-svg__asset--inherit" width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px"><path clip-rule="evenodd" d="M6.414,12.414L3.586,9.586l8-8l2.828,2.828L6.414,12.414z M4.829,14H2l0,0v-2.828l0.586-0.586l2.828,2.828L4.829,14z" fill-rule="evenodd"></path></svg></figure>';
+
 const buttonTextTemplate = text => `
-    <button class="tw-button-icon bttv-font-size-10">
+    <button class="tw-button-icon bttv-font-size-1">
         <span class="tw-button__text">
             ${text}
+        </span>
+    </button>
+`;
+
+const buttonIconTemplate = svg => `
+    <button class="tw-button-icon">
+        <span class="tw-button-icon__icon">
+            ${svg}
         </span>
     </button>
 `;
@@ -63,21 +86,38 @@ const textButton = (action, actionVal, tooltip, text) => `
     ${buttonWrapperTemplate(action, actionVal, tooltip, buttonTextTemplate(text))}
 `;
 
+const iconButton = (action, actionVal, tooltip, iconSvg) => `
+    ${buttonWrapperTemplate(action, actionVal, tooltip, buttonIconTemplate(iconSvg))}
+`;
+
 const modCardsTemplate = `
 <div class="tw-c-background-alt-2 tw-full-width tw-flex" id="${BTTV_MOD_CARDS_ID}">
-    <div class="tw-pd-l-1 tw-inline-flex tw-flex-row" id="${BTTV_MOD_SECTION_ID}">
+    <div class="tw-pd-l-1 tw-inline-flex tw-flex-row ${BTTV_MOD_SECTION_CLASS}">
         <div class="tw-inline-flex">
             ${textButton(ACTIONS.TIMEOUT, 1, 'Purge', '1s')}
             ${textButton(ACTIONS.TIMEOUT, 60, 'Timeout 1m', '1m')}
             ${textButton(ACTIONS.TIMEOUT, 600, 'Timeout 10m', '10m')}
-            ${textButton(ACTIONS.TIMEOUT, 3600, 'Timeout 1hr', '1hr')}
-            ${textButton(ACTIONS.TIMEOUT, 24 * 3600, 'Timeout 24hr', '24hr')}
+            ${textButton(ACTIONS.TIMEOUT, 3600, 'Timeout 1hr', '1h')}
+            ${textButton(ACTIONS.TIMEOUT, 8 * 3600, 'Timeout 8hr', '8h')}
+            ${textButton(ACTIONS.TIMEOUT, 24 * 3600, 'Timeout 24hr', '24h')}
             ${textButton(ACTIONS.PERMIT, null, '!permit User', '!permit')}
+        </div>
+    </div>
+    <div class="tw-c-background-alt-2 tw-full-width tw-flex ${BTTV_MOD_SECTION_CLASS}">
+        <div class="tw-pd-l-1 tw-inline-flex tw-flex-row">
+            <div class="tw-inline-flex">
+                ${iconButton(ACTIONS.UNBAN, null, 'Unban/Untimeout User', svgCheck)}
+                ${iconButton(ACTIONS.BAN, null, 'Ban User', svgBlock)}
+            </div>
+            <div class="tw-inline-flex ${BTTV_OWNER_SECTION_CLASS}">
+                ${textButton(ACTIONS.MOD, null, 'Mod User', 'Mod')}
+                ${textButton(ACTIONS.UNMOD, null, 'Unmod User', 'Unmod')}
+            </div>
         </div>
     </div>
     <div class="tw-pd-l-1 tw-inline-flex tw-flex-row">
         <div class="tw-inline-flex">
-            ${textButton(ACTIONS.MESSAGES, null, 'Show chat messages', 'Messages')}
+            ${textButton(ACTIONS.MESSAGES, null, 'Show chat messages', 'Chat Messages')}
         </div>
     </div>
     <div class="tw-inline-flex tw-flex-row" id="${BTTV_USER_MESSAGES_ID}">
@@ -86,10 +126,6 @@ const modCardsTemplate = `
         </div>
     </div>
 </div>`;
-
-const svgHeart = '<figure class="tw-svg"><svg class="tw-svg__asset tw-svg__asset--heart tw-svg__asset--inherit" width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px"><path clip-rule="evenodd" d="M8,14L1,7V4l2-2h3l2,2l2-2h3l2,2v3L8,14z" fill-rule="evenodd"></path></svg></figure>';
-const svgEye = '<figure class="tw-svg"><svg class="tw-svg__asset tw-svg__asset--glyphviews tw-svg__asset--inherit" width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px"><path clip-rule="evenodd" d="M11,13H5L1,9V8V7l4-4h6l4,4v1v1L11,13z M8,5C6.344,5,5,6.343,5,8c0,1.656,1.344,3,3,3c1.657,0,3-1.344,3-3C11,6.343,9.657,5,8,5z M8,9C7.447,9,7,8.552,7,8s0.447-1,1-1s1,0.448,1,1S8.553,9,8,9z" fill-rule="evenodd"></path></svg></figure>';
-const svgPen = '<figure class="tw-svg"><svg class="tw-svg__asset tw-svg__asset--edit tw-svg__asset--inherit" width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px"><path clip-rule="evenodd" d="M6.414,12.414L3.586,9.586l8-8l2.828,2.828L6.414,12.414z M4.829,14H2l0,0v-2.828l0.586-0.586l2.828,2.828L4.829,14z" fill-rule="evenodd"></path></svg></figure>';
 
 const viewStatTemplate = `
 <div class="tw-flex tw-full-width tw-pd-l-1 bttv-stats">
@@ -239,21 +275,23 @@ class ChatModCardsModule {
         this.lazyRender(() => {
             dragDomElement($(VIEWER_CARD)[0], '.viewer-card');
 
-            this.renderModCards(canModTargetUser);
+            this.renderModCards(currentIsOwner, canModTargetUser);
             this.renderNicknameButton();
             this.renderStats();
             this.renderCreatedDate();
         });
     }
 
-    renderModCards(canModTargetUser) {
+    renderModCards(currentIsOwner, canModTargetUser) {
         let $modCards = $(BTTV_MOD_CARDS_SELECTOR);
         if ($modCards.length === 0) {
+            $(ROW_CONTAINER_SELECTOR).children().last().remove(); // remove twitch mod cards.
             $modCards = $(modCardsTemplate);
             $modCards.appendTo(ROW_CONTAINER_SELECTOR);
         }
 
-        $(`#${BTTV_MOD_SECTION_ID}`).toggleClass(BTTV_HIDE_SECTION_CLASS, !canModTargetUser);
+        $(`.${BTTV_OWNER_SECTION_CLASS}`).toggleClass(BTTV_HIDE_SECTION_CLASS, !currentIsOwner);
+        $(`.${BTTV_MOD_SECTION_CLASS}`).toggleClass(BTTV_HIDE_SECTION_CLASS, !canModTargetUser);
         $(`#${BTTV_USER_MESSAGES_ID}`).toggleClass(BTTV_HIDE_SECTION_CLASS, true);
     }
 
@@ -276,7 +314,7 @@ class ChatModCardsModule {
             $el = $(createdTemplate);
             $el.appendTo('.viewer-card__display-name');
         }
-        $el.text('');
+        $el.text('Loading...');
         this.targetUser.dataPromise.then(data => {
             $el.text(`Created ${moment(data.created_at).format('MMM D, YYYY')}`);
         });
