8228516a2d48a744738a740d69f9549fe7a466b9
J.D. Purcell <jdpurcell@gmail.com>
2018-01-05 01:54:03 -0500

    ability to change VOD playback speed (#2590)

--- src/modules/video_player/index.js
+++ src/modules/video_player/index.js
@@ -2,24 +2,44 @@ const $ = require('jquery');
 const watcher = require('../../watcher');
 const settings = require('../../settings');
 const keyCodes = require('../../utils/keycodes');
+const twitch = require('../../utils/twitch');
 
 const VIDEO_PLAYER_SELECTOR = '.video-player .player';
 
-function handleKeyEvent(keyup) {
-    if (keyup.ctrlKey || keyup.metaKey || keyup.shiftKey) return;
+function stepPlaybackSpeed(faster) {
+    const currentPlayer = twitch.getCurrentPlayer();
+    if (!currentPlayer || !currentPlayer.props.vodID) return;
+    const rates = [ 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 2.0 ];
+    let idx = rates.indexOf(currentPlayer.player.getPlaybackRate());
+    if (idx === -1) return;
+    idx += faster ? 1 : -1;
+    if (idx < 0 || idx >= rates.length) return;
+    currentPlayer.player.setPlaybackRate(rates[idx]);
+}
+
+function handleKeyEvent(keypress) {
+    if (keypress.ctrlKey || keypress.metaKey) return;
     if ($('input, textarea, select').is(':focus')) return;
 
     const $player = $(VIDEO_PLAYER_SELECTOR);
     if (!$player.length) return;
 
-    switch (keyup.keyCode) {
-        case keyCodes.k:
+    switch (keypress.keyCode) {
+        case keyCodes.KeyPress.LessThan:
+        case keyCodes.KeyPress.Comma:
+            stepPlaybackSpeed(false);
+            break;
+        case keyCodes.KeyPress.GreaterThan:
+        case keyCodes.KeyPress.Period:
+            stepPlaybackSpeed(true);
+            break;
+        case keyCodes.KeyPress.k:
             $player.find('.qa-pause-play-button').click();
             break;
-        case keyCodes.f:
+        case keyCodes.KeyPress.f:
             $player.find('.qa-fullscreen-button').click();
             break;
-        case keyCodes.m:
+        case keyCodes.KeyPress.m:
             $player.find('.qa-control-volume').click();
             break;
     }
@@ -64,7 +84,7 @@ class VideoPlayerModule {
     }
 
     keybinds() {
-        $(document).on('keyup.playerControls', handleKeyEvent);
+        $(document).on('keypress.playerControls', handleKeyEvent);
     }
 
     clickToPause() {
