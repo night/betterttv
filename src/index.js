90c52b3f2088acede1e012996cd7cd786cab19fb
Night <night@nightdev.com>
2018-01-28 02:21:17 -0800

    bump deps + version + clear out old sentry ignores we've not been getting many errors, im suspicious

--- src/index.js
+++ src/index.js
@@ -29,49 +29,14 @@
             {
                 release: process.env.GIT_REV,
                 environment: process.env.NODE_ENV,
-                ignoreErrors: [
-                    'Blocked a frame with origin',
-                    'player-core-min',
-                    'InvalidAccessError',
-                    'wrapped(betterttv)',
-                    'Access is denied.',
-                    'container.className.match is not a function',
-                    'this exception cannot be caught',
-                    'Wrong length!',
-                    'Cannot read property \'value\' of undefined',
-                    '"onJsReady"',
-                    'Can\'t execute code from a freed script',
-                    /^<anonymous> in/,
-                    /^null$/,
-                    /^undefined$/,
-                    // Users that have broken jQuery
-                    'this.dom.draggable is not a function',
-                    'Cannot read property \'autoNS\' of undefined',
-                    'Cannot read property \'autoWE\' of undefined',
-                    'tipsy is not a function',
-                    'tipsy is undefined',
-                    'draggable is not a function',
-                    'draggable is undefined',
-                    'jQuery is not a function',
-                    'unsupported pseudo: hover',
-                    // Emote Menu
-                    'Getter already exists.',
-                    'Cannot read property \'setChannelName\' of undefined',
-                    '.getTime is not a function',
-                    '`callback` must be a function.'
-                ],
+                ignoreErrors: [],
                 whitelistUrls: [
                     /betterttv\.js/,
                     /\.betterttv\.net/
                 ],
                 shouldSendCallback: data => {
                     const exception = data.exception && data.exception.values[0];
-                    if (data.message && data.message.includes('betterttv.js in wrap')) return false;
-                    if (data.message === 'out of memory') return;
-                    if (['betterttv in apply', 'wrapped(betterttv)'].includes(data.culprit)) return false;
-                    if (exception && !exception.value) return false;
-                    if (exception && ['NS_ERROR_NOT_INITIALIZED', 'NS_ERROR_OUT_OF_MEMORY', 'NS_ERROR_FAILURE'].includes(exception.type)) return true;
-                    if (data.exception && data.exception.values.length < 3) return false;
+                    if (exception && ['NS_ERROR_NOT_INITIALIZED', 'NS_ERROR_OUT_OF_MEMORY', 'NS_ERROR_FAILURE'].includes(exception.type)) return false;
                     return true;
                 }
             }
