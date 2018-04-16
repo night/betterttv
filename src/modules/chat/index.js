4f36d4009fa9499f270ee72794ff21db0215591e
Smatify <Smatify@users.noreply.github.com>
2018-04-16 08:30:44 +0200

    Removed parent selector

--- src/modules/chat/index.js
+++ src/modules/chat/index.js
@@ -224,7 +224,7 @@ class ChatModule {
             $element.hide();
         }
 
-        const $modIcons = $element.find('.mod-icon').parent();
+        const $modIcons = $element.find('.mod-icon');
         if ($modIcons.length) {
             const userIsOwner = twitch.getUserIsOwnerFromTagsBadges(user.badges);
             const userIsMod = twitch.getUserIsModeratorFromTagsBadges(user.badges);
