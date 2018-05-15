d09c5118a274ba815dff0625c0a9451e70018745
Nareese <Nareese@users.noreply.github.com>
2018-05-15 17:02:50 -0400

    Additional chat events handling
    Add additional chat events handling to fix ritual hiding, and implement subscription hiding.  Replaces the hide_new_viewer_greeting module which twitch broke by making the CSS class on rituals the same as other notices.
    
    Fixes #3060
    Fixes #3104

---
+++ src/modules/hide_chat_events/index.js
@@ -0,0 +1,43 @@
+const settings = require('../../settings');
+const watcher = require('../../watcher');
+const twitch = require('../../utils/twitch');
+
+class HideChatEventsModule {
+    constructor() {
+        settings.add({
+            id: 'hideNewViewerGreeting',
+            name: 'Hide New Viewer Greeting',
+            defaultValue: false,
+            description: 'Hides the new viewer greeting message from the chat'
+        });
+        settings.add({
+            id: 'hideSubscriptionNotices',
+            name: 'Hide Subscription and Resubscription notices',
+            defaultValue: false,
+            description: 'Hides subs, re-subs, and gift subs from the chat'
+        });
+
+        watcher.on('chat.buffer.event', event => {
+            this.handleBufferEvent(event);
+        });
+    }
+
+    handleBufferEvent({event, preventDefault}) {
+        switch (event.type) {
+            case twitch.TMIActionTypes.RITUAL:
+                if (settings.get('hideNewViewerGreeting')) {
+                    preventDefault();
+                }
+                break;
+            case twitch.TMIActionTypes.SUBSCRIPTION:
+            case twitch.TMIActionTypes.RESUBSCRIPTION:
+            case twitch.TMIActionTypes.SUBGIFT:
+                if (settings.get('hideSubscriptionNotices')) {
+                    preventDefault();
+                }
+                break;
+        }
+    }
+}
+
+module.exports = new HideChatEventsModule();
