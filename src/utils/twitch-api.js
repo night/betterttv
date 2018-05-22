d053f6d4944bb442c0df37a73a7677b666f8af43
Zack <night@users.noreply.github.com>
2018-05-21 22:39:07 -0700

    Update twitch-api.js

--- src/utils/twitch-api.js
+++ src/utils/twitch-api.js
@@ -2,14 +2,16 @@ const $ = require('jquery');
 const querystring = require('querystring');
 
 const API_ENDPOINT = 'https://api.twitch.tv/v5/';
+const GQL_ENDPOINT = 'https://gql.twitch.tv/gql';
 const CLIENT_ID = '6x8avioex0zt85ht6py4sq55z6avsea';
 
 let accessToken;
 
 function request(method, path, options = {}) {
+    const url = options.url || `${API_ENDPOINT}${path}${options.qs ? `?${querystring.stringify(options.qs)}` : ''}`
     return new Promise((resolve, reject) => {
         $.ajax({
-            url: `${API_ENDPOINT}${path}${options.qs ? `?${querystring.stringify(options.qs)}` : ''}`,
+            url,
             method,
             dataType: 'json',
             data: options.body ? JSON.stringify(options.body) : undefined,
@@ -30,6 +32,13 @@ function request(method, path, options = {}) {
 module.exports = {
     setAccessToken(newAccessToken) {
         accessToken = newAccessToken;
+    },
+    
+    graphqlQuery(query) {
+        return request('POST', null, {
+            url: GQL_ENDPOINT,
+            body: {query}
+        });
     },
 
     get(path, options) {
