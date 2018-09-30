8eb8cbed2131be8bee19ee5b60e5a3cb0b18963e
Night <night@nightdev.com>
2018-09-30 13:47:23 -0700

    fixes #3283

--- src/modules/disable_host_mode/index.js
+++ src/modules/disable_host_mode/index.js
@@ -5,19 +5,19 @@ const twitch = require('../../utils/twitch');
 let oldGetHostedChannelLogin;
 function newGetHostedChannelLogin() {
     if (settings.get('disableHostMode')) return null;
-    oldGetHostedChannelLogin.call(this, ...arguments);
+    return oldGetHostedChannelLogin.call(this, ...arguments);
 }
 
 let oldHandleHostingChange;
 function newHandleHostingChange(e) {
     if (e !== null && settings.get('disableHostMode')) return null;
-    oldHandleHostingChange.call(this, ...arguments);
+    return oldHandleHostingChange.call(this, ...arguments);
 }
 
 let oldOnChatHostingChange;
 function newOnChatHostingChange() {
     if (settings.get('disableHostMode')) return null;
-    oldOnChatHostingChange.call(this, ...arguments);
+    return oldOnChatHostingChange.call(this, ...arguments);
 }
 
 let lastPlayer;
