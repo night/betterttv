670971c6688ca299203c751557830b2542e653f3
Night <night@nightdev.com>
2019-02-10 19:52:56 -0800

    bump version and improve reversed chat direction

--- src/modules/chat_direction/index.js
+++ src/modules/chat_direction/index.js
@@ -1,22 +1,37 @@
 const $ = require('jquery');
 const settings = require('../../settings');
+const twitch = require('../../utils/twitch');
 
-const CHAT_LINE_SELECTOR = '.chat-list__lines .chat-line__message';
+const CHAT_LIST_SCROLL_CONTENT = '.chat-list .chat-list__lines .simplebar-scroll-content';
 
 class ChatDirection {
     constructor() {
         settings.add({
-            id: 'chatDirection',
+            id: 'reverseChatDirection',
             name: 'Reverse Chat Messages Direction',
             defaultValue: false,
             description: 'New chat messages come from the top'
         });
-        settings.on('changed.chatDirection', () => this.toggleChatDirection());
+        settings.on('changed.reverseChatDirection', () => this.toggleChatDirection());
+        settings.on('load.chat', () => this.toggleChatAutoScrolling());
         this.toggleChatDirection();
     }
 
+    toggleChatAutoScrolling() {
+        const scroller = twitch.getChatScroller();
+        const reverseChatDirection = settings.get('reverseChatDirection');
+        if (!scroller) return;
+        scroller.setState({
+            isAutoScrolling: !reverseChatDirection
+        });
+        if (reverseChatDirection) {
+            $(CHAT_LIST_SCROLL_CONTENT)[0].scrollTop = 0;
+        }
+    }
+
     toggleChatDirection() {
-        $(CHAT_LINE_SELECTOR).parent().toggleClass('bttv-chat-direction-reversed', settings.get('chatDirection'));
+        $('body').toggleClass('bttv-chat-direction-reversed', settings.get('reverseChatDirection'));
+        this.toggleChatAutoScrolling();
     }
 }
 
