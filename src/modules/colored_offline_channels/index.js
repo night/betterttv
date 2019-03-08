522050f367dfeca8a9e81c3c8232e1a30d8a9d5b
itsazhuhere <andrez@uci.edu>
2019-03-07 16:58:16 -0800

    added functionality to add colors back to offline channels

---
+++ src/modules/colored_offline_channels/index.js
@@ -0,0 +1,47 @@
+const $ = require('jquery');
+const watcher = require('../../watcher');
+const settings = require('../../settings');
+
+class ColoredOfflineChannelsModule {
+    constructor() {
+        settings.add({
+            id: 'coloredOfflineChannels',
+            name: 'Colored Offline Channels',
+            defaultValue: true,
+            description: 'All offline channels will now have colored icons'
+        });
+        settings.on('changed.coloredOfflineChannels', () => this.toggleColoredOffline());
+        watcher.on('load', () => {
+            this.toggleColoredOffline();
+            this.channelListObserver();
+        });
+    }
+
+    toggleColoredOffline() {
+        console.log('triggered');
+        if (settings.get('coloredOfflineChannels')) {
+            $('.side-nav-card__avatar').toggleClass('side-nav-card__avatar--offline', false);
+        } else {
+            $('.side-nav-card__avatar').each( (ind, el) => {
+                const status = $(el).parent().find('.side-nav-card__metadata div span').text();
+                if (status === 'Offline') {
+                    $(el).toggleClass('side-nav-card__avatar--offline', true);
+                }
+            });
+        }
+    }
+
+    channelListObserver() {
+        const chatList = $('.tw-link--button[data-a-target="side-nav-show-more-button"]').parent().parent()[0];
+        const channelListWatcher = new window.MutationObserver(mutations =>
+            mutations.forEach(mutation => {
+                if (mutation.addedNodes.length > 0) {
+                    this.toggleColoredOffline();
+                }
+            })
+        );
+        channelListWatcher.observe(chatList, {childList: true});
+    }
+}
+
+module.exports = new ColoredOfflineChannelsModule();
