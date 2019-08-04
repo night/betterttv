366fc50e9582bad47868c7fd9e9dc0c63a9769bd
hristov296 <hristov296@gmail.com>
2019-08-04 20:11:16 +0300

    chat in fullscreen module

---
+++ src/modules/chat_in_fullscreen/index.js
@@ -0,0 +1,42 @@
+const $ = require('jquery');
+const settings = require('../../settings');
+
+class ChatInFullscreen {
+    constructor() {
+        settings.add({
+            id: 'chatInFullscreen',
+            name: 'Chat in Fullscreen',
+            defaultValue: false,
+            description: 'Shows chat section while in fullscreen mode.'
+        });
+        this.chatFsActive = false;
+        settings.on('changed.chatInFullscreen', () => this.toggleChatInFullscreen());
+        $(document).on('fullscreenchange mozfullscreenchange MSFullscreenChange webkitfullscreenchange', () => this.onFullScreenMoveChat());
+        this.toggleChatInFullscreen();
+    }
+
+    toggleChatInFullscreen() {
+        this.chatFsActive = settings.get('chatInFullscreen');
+        if (this.chatFsActive) {
+            $('body').addClass('bttv-chat-in-fullscreen');
+            $('.video-player__container').wrapInner('<div class="chat-in-fs-wrap" />');
+        } else {
+            $('body').removeClass('bttv-chat-in-fullscreen');
+            $('.video-player__container > .chat-in-fs-wrap').contents().unwrap();
+        }
+    }
+
+    onFullScreenMoveChat() {
+        const fsElement = document.fullscreenElement || document.mozFullScreenElement || document.msFullscreenElement || document.webkitFullscreenElement;
+
+        if (fsElement && this.chatFsActive) {
+            $('body').addClass('fullscreen-active');
+            $('.right-column .channel-root__right-column').appendTo(fsElement);
+        } else {
+            $('body').removeClass('fullscreen-active');
+            $('.channel-root__right-column').appendTo('.right-column > .tw-block');
+        }
+    }
+}
+
+module.exports = new ChatInFullscreen();
