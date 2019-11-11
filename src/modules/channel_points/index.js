8850d514d2931108259e99f44b32156729f4feeb
whitecrownclown <daniel.andrei@ezugi.com>
2019-11-11 12:41:57 +0200

    feat: add toggle for automatic claim of bonus pts

--- src/modules/channel_points/index.js
+++ src/modules/channel_points/index.js
@@ -1,26 +1,39 @@
 const $ = require('jquery');
 const watcher = require('../../watcher');
+const settings = require('../../settings');
 
 const containerSelector = '.chat-input__buttons-container';
 const claimButtonSelector = '.claimable-bonus__icon';
 
+const bonusPointsObserver = new window.MutationObserver(mutations =>
+    mutations.forEach(mutation => {
+        for (const el of mutation.addedNodes) {
+            const $el = $(el);
+
+            $el.find(claimButtonSelector).click();
+        }
+    })
+);
+
 class ChannelPoints {
     constructor() {
+        settings.add({
+            id: 'autoClaimBonusPoints',
+            name: 'Automatic Claim of Bonus Points',
+            defaultValue: false,
+            description: 'Automatically claim channel bonus points'
+        });
+        settings.on('changed.autoClaimBonusPoints', () => this.load());
         watcher.on('load.channel', () => this.load());
     }
 
     load() {
+        if (!settings.get('autoClaimBonusPoints')) {
+            bonusPointsObserver.disconnect();
+            return;
+        }
+
         $(claimButtonSelector).click();
-
-        const bonusPointsObserver = new window.MutationObserver(mutations =>
-            mutations.forEach(mutation => {
-                for (const el of mutation.addedNodes) {
-                    const $el = $(el);
-
-                    $el.find(claimButtonSelector).click();
-                }
-            })
-        );
 
         const observe = (_watcher, element) => {
             if (!element) return;
