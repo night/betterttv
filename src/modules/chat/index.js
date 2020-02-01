ec38fb0f7d2e2cc209c2e8b49222bee1954b0dab
Treee <jonathon.lindemann@gmail.com>
2020-02-01 16:55:36 -0700

    removing unrelated formatting change

--- src/modules/chat/index.js
+++ src/modules/chat/index.js
@@ -68,6 +68,7 @@ class ChatModule {
                 if (!user) {
                     continue;
                 }
+
                 this.messageReplacer($(element), user);
             }
         });
