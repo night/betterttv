cef2d3c7df27db83e7c6c136dd907cce22c16758
Adam Sinclair <adam0sinclair@icloud.com>
2020-04-20 16:54:45 +0100

    Implement message tracing in channel chat
    Includes:
    - settings element to turn feature on or off
    - settings element to make mentions more visible
    - cancelling tracing by clicking elsewhere in any message

---
+++ src/modules/chat_trace_mentions/index.js
@@ -0,0 +1,107 @@
+const $ = require('jquery');
+const watcher = require('../../watcher');
+const debug = require('../../utils/debug');
+const settings = require('../../settings');
+
+const CHAT_MESSAGE_LINE_SELECTOR = 'div.chat-line__message';
+
+function clearMarks() {
+    $(`${CHAT_MESSAGE_LINE_SELECTOR}`).removeClass('bttv-mentioned bttv-mention-sender');
+}
+
+function markMentioned(mentioned) {
+    $(`${CHAT_MESSAGE_LINE_SELECTOR}[data-bttv-sender="${mentioned}"]`).addClass('bttv-mentioned');
+}
+
+function markSender(sender) {
+    $(`${CHAT_MESSAGE_LINE_SELECTOR}[data-bttv-sender="${sender}"]`).addClass('bttv-mention-sender');
+}
+
+let currentMentioned = null;
+let currentSender = null;
+class ChatTraceMentionsModule {
+    constructor() {
+        watcher.on('chat.message', ($message, messageObj) => this.onMessage($message, messageObj));
+
+        settings.add({
+            id: 'traceMentions',
+            name: 'Trace Mentions',
+            defaultValue: false,
+            description: 'Click on a @mention in chat to highlight messages from the mentioned and the mentioner',
+        });
+        settings.on('changed.traceMentions', () => {
+            currentMentioned = null;
+            currentSender = null;
+            clearMarks();
+            this.styleMentions();
+        });
+
+        settings.add({
+            id: 'traceMentionsMoreVisible',
+            name: 'Trace Mentions: More Visible Mentions',
+            defaultValue: false,
+            description: 'Make mentions stand out more and use the \'pointer\' cursor'
+        });
+        settings.on('changed.traceMentionsMoreVisible', () => this.styleMentions());
+
+        this.listenForMentionClicks();
+        this.listenForTextClicks();
+    }
+
+    onMessage($message, {user}) {
+        // data tag it with its sender
+        const sender = user.userLogin.toLowerCase();
+        this.dataTagSender($message, sender);
+
+        if (settings.get('traceMentions')) {
+            // if the sender is the mentioned or mention-sender, mark it
+            if (currentMentioned === sender) $message.addClass('bttv-mentioned');
+            if (currentSender === sender) $message.addClass('bttv-mention-sender');
+            if (settings.get('traceMentionsMoreVisible')) {
+                $message.find('.mention-fragment').addClass('bttv-trace-mention-fragment');
+            }
+        }
+    }
+
+    dataTagSender($message, sender) {
+        $message.attr('data-bttv-sender', sender);
+    }
+
+    listenForMentionClicks() {
+        $(document).on('click', '.mention-fragment', function(event) {
+            const mention = $(this).text().toLowerCase();
+            const sender = $(this).parent(`${CHAT_MESSAGE_LINE_SELECTOR}`).data('bttv-sender');
+            debug.log(`Clicked on a mention span for ${mention} sent by ${sender}.`);
+            const mentioned = mention.replace('@', '');
+            clearMarks();
+            if (settings.get('traceMentions')) {
+                markMentioned(mentioned);
+                markSender(sender);
+            }
+            currentMentioned = mentioned;
+            currentSender = sender;
+            event.stopPropagation();
+        });
+    }
+
+    listenForTextClicks() {
+        $(document).on('click', `${CHAT_MESSAGE_LINE_SELECTOR}`, () => {
+            debug.log('Clicked elsewhere in a message.');
+            clearMarks();
+            currentMentioned = null;
+            currentSender = null;
+        });
+    }
+
+    styleMentions() {
+        if (settings.get('traceMentions') && settings.get('traceMentionsMoreVisible')) {
+            // apply .bttv-trace-mention-fragment to all .mention-fragment
+            $('.mention-fragment').addClass('bttv-trace-mention-fragment');
+        } else {
+            // remove .bttv-trace-mention-fragment from all .bttv-trace-mention-fragment
+            $('.mention-fragment.bttv-trace-mention-fragment').removeClass('bttv-trace-mention-fragment');
+        }
+    }
+}
+
+module.exports = new ChatTraceMentionsModule();
