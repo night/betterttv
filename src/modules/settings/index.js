0f08217d9eeae0ec12118b4c84669019323ad982
Night <night@nightdev.com>
2020-08-30 03:52:51 -0700

    fixes user dropdown settings icon

--- src/modules/settings/index.js
+++ src/modules/settings/index.js
@@ -197,13 +197,13 @@ class SettingsModule {
 
         $('a[data-a-target="settings-dropdown-link"]').parent('div.tw-full-width.tw-relative').after(`
             <div class="tw-full-width tw-relative">
-                <a title="BetterTTV Settings" class="tw-block tw-border-radius-medium tw-full-width tw-interactable--alpha tw-interactable--hover-enabled tw-interactable tw-interactive bttvSettingsDropDown" href="#">
+                <a title="BetterTTV Settings" class="tw-block tw-border-radius-medium tw-full-width tw-interactable--default tw-interactable--hover-enabled tw-interactable tw-interactive bttvSettingsDropDown" href="#">
                     <div class="tw-align-items-center tw-flex tw-pd-05 tw-relative">
                         <div class="tw-align-items-center tw-flex tw-flex-shrink-0 tw-pd-r-05">
                             <div class="tw-align-items-center tw-drop-down-menu-item-figure tw-flex">
                                 <div class="bttvSettingsIconContainer tw-align-items-center tw-icon tw-inline-flex">
                                     <div class="tw-aspect tw-aspect--align-top">
-                                        <div class="tw-aspect__spacer" style="padding-bottom: 100%;"></div>
+                                        <div class="tw-aspect__spacer"></div>
                                         <figure class="icon bttvSettingsIconDropDown"></figure>
                                     </div>
                                 </div>
