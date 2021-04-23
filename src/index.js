6f3d57a3e7e315944642553f74546d0e44d4e133
Night <night@nightdev.com>
2021-04-23 16:00:38 -0700

    more sentry filtering

--- src/index.js
+++ src/index.js
@@ -25,32 +25,36 @@
   const Sentry = await import('@sentry/browser');
   const {Dedupe: DedupeIntegration} = await import('@sentry/integrations');
 
+  const ignoreErrors = [
+    'InvalidAccessError',
+    'out of memory',
+    'InvalidStateError',
+    'QuotaExceededError',
+    'NotFoundError',
+    'SecurityError',
+    'AbortError',
+    'TypeMismatchError',
+    'HierarchyRequestError',
+    'IndexSizeError',
+    /^undefined$/,
+    `Unexpected token '<'`,
+    'prompt() is and will not be supported.',
+    'ChunkLoadError',
+    'Loading CSS chunk',
+    'Loading chunk',
+    'Failed to fetch',
+    'NetworkError',
+    'getAnimatedEmoteMode is not a function',
+    'can\'t redefine non-configurable property "userAgent"',
+    '$ is not defined',
+  ];
+
   Sentry.init({
     release: process.env.GIT_REV,
     environment: process.env.NODE_ENV,
     dsn: process.env.SENTRY_URL,
-    ignoreErrors: [
-      'InvalidAccessError',
-      'out of memory',
-      'InvalidStateError',
-      'QuotaExceededError',
-      'NotFoundError',
-      'SecurityError',
-      'AbortError',
-      'TypeMismatchError',
-      'HierarchyRequestError',
-      'IndexSizeError',
-      /^undefined$/,
-      `Unexpected token '<'`,
-      'prompt() is and will not be supported.',
-      'ChunkLoadError',
-      'Loading CSS chunk',
-      'Loading chunk',
-      'Failed to fetch',
-      'NetworkError',
-      'getAnimatedEmoteMode is not a function',
-    ],
-    allowUrls: [/betterttv\.js/, /\.betterttv\.net/],
+    ignoreErrors,
+    allowUrls: [/betterttv\.js/, /\.betterttv\.net/, /\/betterttv\//i],
     denyUrls: [/static\.twitchcdn\.net\/assets/, /avalon\.js/, /avalon\.[a-zA-Z0-9]+\.js/, /script\.js/],
     integrations: [
       new Sentry.Integrations.GlobalHandlers({
@@ -61,6 +65,34 @@
       }),
       new DedupeIntegration(),
     ],
+    beforeSend: (event) => {
+      const {exception} = event;
+      const exceptionValue = exception != null && exception.values != null && event.exception.values[0];
+      if (exceptionValue != null) {
+        // exceptions without a stacktrace are non-actionable
+        if (exceptionValue.stacktrace == null) {
+          return null;
+        }
+
+        // exceptions with an anonymous stacktrace are non-actionable
+        const {frames} = exceptionValue.stacktrace;
+        if (frames != null && frames[0] != null) {
+          if (frames[0].filename == null || frames[0].filename === '<anonymous>') {
+            return null;
+          }
+        }
+
+        // for whatever reason, ignoreErrors does not work for errors caught by other sentry users
+        // let's just filter them ourselves too
+        const exceptionString = `${exceptionValue.type} ${exceptionValue.value}`;
+        for (const ignoreError of ignoreErrors) {
+          if (typeof ignoreError === 'string' && exceptionString.toLowerCase().includes(ignoreError.toLowerCase())) {
+            return null;
+          }
+        }
+      }
+      return event;
+    },
   });
 
   const {default: cookies} = await import('cookies-js');
