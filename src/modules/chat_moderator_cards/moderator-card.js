0441633dcfb8de46d5b0a13bcceb617edfd2cc66
Night <night@nightdev.com>
2021-05-22 23:13:56 -0700

    update chat moderator card css

--- src/modules/chat_moderator_cards/moderator-card.js
+++ src/modules/chat_moderator_cards/moderator-card.js
@@ -18,13 +18,14 @@ const Commands = {
 };
 
 const Icons = {
-  EYE: '<figure class="tw-svg"><svg class="tw-svg__asset tw-svg__asset--glyphviews tw-svg__asset--inherit" width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px" fill="white"><path clip-rule="evenodd" d="M11,13H5L1,9V8V7l4-4h6l4,4v1v1L11,13z M8,5C6.344,5,5,6.343,5,8c0,1.656,1.344,3,3,3c1.657,0,3-1.344,3-3C11,6.343,9.657,5,8,5z M8,9C7.447,9,7,8.552,7,8s0.447-1,1-1s1,0.448,1,1S8.553,9,8,9z" fill-rule="evenodd"></path></svg></figure>',
+  EYE:
+    '<svg width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px" fill="white"><path clip-rule="evenodd" d="M11,13H5L1,9V8V7l4-4h6l4,4v1v1L11,13z M8,5C6.344,5,5,6.343,5,8c0,1.656,1.344,3,3,3c1.657,0,3-1.344,3-3C11,6.343,9.657,5,8,5z M8,9C7.447,9,7,8.552,7,8s0.447-1,1-1s1,0.448,1,1S8.553,9,8,9z" fill-rule="evenodd"></path></svg>',
   HEART:
-    '<figure class="tw-svg"><svg class="tw-svg__asset tw-svg__asset--heart tw-svg__asset--inherit" width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px" fill="white"><path clip-rule="evenodd" d="M8,14L1,7V4l2-2h3l2,2l2-2h3l2,2v3L8,14z" fill-rule="evenodd"></path></svg></figure>',
+    '<svg width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px" fill="white"><path clip-rule="evenodd" d="M8,14L1,7V4l2-2h3l2,2l2-2h3l2,2v3L8,14z" fill-rule="evenodd"></path></svg></figure>',
   PENCIL:
-    '<figure class="tw-svg"><svg class="tw-svg__asset tw-svg__asset--edit tw-svg__asset--inherit" width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px" fill="white"><path clip-rule="evenodd" d="M6.414,12.414L3.586,9.586l8-8l2.828,2.828L6.414,12.414z M4.829,14H2l0,0v-2.828l0.586-0.586l2.828,2.828L4.829,14z" fill-rule="evenodd"></path></svg></figure>',
+    '<svg width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px" fill="white"><path clip-rule="evenodd" d="M6.414,12.414L3.586,9.586l8-8l2.828,2.828L6.414,12.414z M4.829,14H2l0,0v-2.828l0.586-0.586l2.828,2.828L4.829,14z" fill-rule="evenodd"></path></svg></figure>',
   BIRTHDAY_CAKE:
-    '<figure class="tw-svg"><svg class="tw-svg__asset tw-svg__asset--heart tw-svg__asset--inherit" width="16px" height="16px" viewBox="0 0 1792 1792" fill="white"><path d="M1792 1408v384h-1792v-384q45 0 85-14t59-27.5 47-37.5q30-27 51.5-38t56.5-11q24 0 44 7t31 15 33 27q29 25 47 38t58 27 86 14q45 0 85-14.5t58-27 48-37.5q21-19 32.5-27t31-15 43.5-7q35 0 56.5 11t51.5 38q28 24 47 37.5t59 27.5 85 14 85-14 59-27.5 47-37.5q30-27 51.5-38t56.5-11q34 0 55.5 11t51.5 38q28 24 47 37.5t59 27.5 85 14zm0-320v192q-24 0-44-7t-31-15-33-27q-29-25-47-38t-58-27-85-14q-46 0-86 14t-58 27-47 38q-22 19-33 27t-31 15-44 7q-35 0-56.5-11t-51.5-38q-29-25-47-38t-58-27-86-14q-45 0-85 14.5t-58 27-48 37.5q-21 19-32.5 27t-31 15-43.5 7q-35 0-56.5-11t-51.5-38q-28-24-47-37.5t-59-27.5-85-14q-46 0-86 14t-58 27-47 38q-30 27-51.5 38t-56.5 11v-192q0-80 56-136t136-56h64v-448h256v448h256v-448h256v448h256v-448h256v448h64q80 0 136 56t56 136zm-1280-864q0 77-36 118.5t-92 41.5q-53 0-90.5-37.5t-37.5-90.5q0-29 9.5-51t23.5-34 31-28 31-31.5 23.5-44.5 9.5-67q38 0 83 74t45 150zm512 0q0 77-36 118.5t-92 41.5q-53 0-90.5-37.5t-37.5-90.5q0-29 9.5-51t23.5-34 31-28 31-31.5 23.5-44.5 9.5-67q38 0 83 74t45 150zm512 0q0 77-36 118.5t-92 41.5q-53 0-90.5-37.5t-37.5-90.5q0-29 9.5-51t23.5-34 31-28 31-31.5 23.5-44.5 9.5-67q38 0 83 74t45 150z" /></svg></figure>',
+    '<svg width="16px" height="16px" viewBox="0 0 1792 1792" fill="white"><path d="M1792 1408v384h-1792v-384q45 0 85-14t59-27.5 47-37.5q30-27 51.5-38t56.5-11q24 0 44 7t31 15 33 27q29 25 47 38t58 27 86 14q45 0 85-14.5t58-27 48-37.5q21-19 32.5-27t31-15 43.5-7q35 0 56.5 11t51.5 38q28 24 47 37.5t59 27.5 85 14 85-14 59-27.5 47-37.5q30-27 51.5-38t56.5-11q34 0 55.5 11t51.5 38q28 24 47 37.5t59 27.5 85 14zm0-320v192q-24 0-44-7t-31-15-33-27q-29-25-47-38t-58-27-85-14q-46 0-86 14t-58 27-47 38q-22 19-33 27t-31 15-44 7q-35 0-56.5-11t-51.5-38q-29-25-47-38t-58-27-86-14q-45 0-85 14.5t-58 27-48 37.5q-21 19-32.5 27t-31 15-43.5 7q-35 0-56.5-11t-51.5-38q-28-24-47-37.5t-59-27.5-85-14q-46 0-86 14t-58 27-47 38q-30 27-51.5 38t-56.5 11v-192q0-80 56-136t136-56h64v-448h256v448h256v-448h256v448h256v-448h256v448h64q80 0 136 56t56 136zm-1280-864q0 77-36 118.5t-92 41.5q-53 0-90.5-37.5t-37.5-90.5q0-29 9.5-51t23.5-34 31-28 31-31.5 23.5-44.5 9.5-67q38 0 83 74t45 150zm512 0q0 77-36 118.5t-92 41.5q-53 0-90.5-37.5t-37.5-90.5q0-29 9.5-51t23.5-34 31-28 31-31.5 23.5-44.5 9.5-67q38 0 83 74t45 150zm512 0q0 77-36 118.5t-92 41.5q-53 0-90.5-37.5t-37.5-90.5q0-29 9.5-51t23.5-34 31-28 31-31.5 23.5-44.5 9.5-67q38 0 83 74t45 150z" /></svg></figure>',
 };
 
 const MODERATOR_CARD_DISPLAY_NAME_SELECTOR = '.viewer-card-header__display-name h4';
@@ -33,11 +34,11 @@ const MODERATOR_CARD_ACTIONS_SELECTOR = 'button[data-test-selector="ban-button"]
 const CHAT_INPUT_SELECTOR = 'textarea[data-a-target="chat-input"]';
 
 const moderatorActionButtonTemplate = (command, duration, tooltipText, buttonText) => `
-  <div class="bttv-tooltip-wrapper tw-inline-flex">
+  <div class="bttv-tooltip-wrapper">
     <div class="bttv-moderator-card-action" data-command="${html.escape(command)}" data-duration="${
   html.escape(duration) || ''
 }">
-      <button class="tw-button__text">${html.escape(buttonText)}</button>
+      <button class="actionText">${html.escape(buttonText)}</button>
     </div>
     <div class="bttv-tooltip bttv-tooltip--up bttv-tooltip--align-center">${html.escape(tooltipText)}</div>
   </div>
@@ -55,24 +56,22 @@ const MODERATOR_ACTIONS_TEMPLATE = `
 `;
 
 const userStatsItemTemplate = (icon, value) => `
-  <div class="tw-align-items-center tw-inline-flex tw-stat tw-pd-l-1">
-    <div class="tw-align-items-center tw-inline-flex tw-stat__icon tw-mg-r-1">${icon}</div>
-    <div class="tw-stat__value">${html.escape(value)}</div>
+  <div class="userStat">
+    <div class="statIcon">${icon}</div>
+    <div>${html.escape(value)}</div>
   </div>
 `;
 
 const userStatsTemplate = (views, follows, createdAt) => `
   <div class="bttv-moderator-card-user-stats">
-    <div class="tw-flex tw-full-width">
-      ${userStatsItemTemplate(Icons.EYE, views.toLocaleString())}
-      ${userStatsItemTemplate(Icons.HEART, follows.toLocaleString())}
-      ${userStatsItemTemplate(Icons.BIRTHDAY_CAKE, dayjs(createdAt).format('MMM D, YYYY'))}
-    </div>
+    ${userStatsItemTemplate(Icons.EYE, views.toLocaleString())}
+    ${userStatsItemTemplate(Icons.HEART, follows.toLocaleString())}
+    ${userStatsItemTemplate(Icons.BIRTHDAY_CAKE, dayjs(createdAt).format('MMM D, YYYY'))}
   </div>
 `;
 
 const userMessagesTemplate = (messagesHTML) => `
-  <div class="bttv-moderator-card-messages tw-c-background-base">
+  <div class="bttv-moderator-card-messages">
     <div class="label">
       <span>Chat Messages</span>
       <div class="triangle"></div>
@@ -84,8 +83,8 @@ const userMessagesTemplate = (messagesHTML) => `
 `;
 
 const NICKNAME_CHANGE_BUTTON_TEMPLATE = `
-  <button class="tw-button-icon tw-button-icon--overlay bttv-moderator-card-nickname-change-button">
-    <span class="tw-button-icon__icon">${Icons.PENCIL}</span>
+  <button class="bttv-moderator-card-nickname-change-button">
+    <span class="buttonIcon">${Icons.PENCIL}</span>
   </button>
 `;
 
@@ -178,7 +177,7 @@ class ModeratorCard {
 
     const $moderatorActions = this.$element
       .find('button[data-a-target="viewer-card-more-button"]')
-      .closest('div.tw-c-background-base');
+      .closest('div.viewer-card');
     const $messages = $(userMessagesTemplate(this.messages.map(({outerHTML}) => outerHTML)));
     $moderatorActions.after($messages);
 
