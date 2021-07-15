27f3e62016d8e47496a49d27dade67e08d5bec33
dclstn <8022520-dclstn@users.noreply.gitlab.com>
2021-07-15 19:30:28 +0100

    fix error message

--- dev/post-install.js
+++ dev/post-install.js
@@ -2,6 +2,6 @@ import {writeFileSync} from 'fs';
 
 writeFileSync('./node_modules/rsuite/lib/styles/normalize.less', '', (err) => {
   if (err) {
-    throw new Error('Post-install failed, could not locate normalize.less');
+    throw new Error(`Post-install failed: ${err}`);
   }
 });
