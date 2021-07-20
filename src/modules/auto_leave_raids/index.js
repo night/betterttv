59e77636c06339a5f1bc88787190368280626083
bkmak <33822064+bkmak@users.noreply.github.com>
2021-07-20 14:03:27 -0400

    Linting

--- src/modules/auto_leave_raids/index.js
+++ src/modules/auto_leave_raids/index.js
@@ -2,44 +2,44 @@ import $ from 'jquery';
 import watcher from '../../watcher.js';
 import settings from '../../settings.js';
 import domObserver from '../../observers/dom.js';
-import { SettingIds } from '../../constants.js';
+import {SettingIds} from '../../constants.js';
 
 const RAID_BANNER_SELECTOR = '[data-test-selector="raid-banner"]';
 
 let raidListener;
 
 class AutoLeaveRaidsModule {
-    constructor() {
-        watcher.on('load.chat', () => this.load());
-        settings.on(`changed.${SettingIds.AUTO_LEAVE_RAIDS}`, () => this.load());
+  constructor() {
+    watcher.on('load.chat', () => this.load());
+    settings.on(`changed.${SettingIds.AUTO_LEAVE_RAIDS}`, () => this.load());
+  }
+
+  load() {
+    if (settings.get(SettingIds.AUTO_LEAVE_RAIDS)) {
+      if (raidListener) return;
+
+      raidListener = domObserver.on(RAID_BANNER_SELECTOR, (node, isConnected) => this.handleLeave(node, isConnected));
+
+      return;
     }
 
-    load() {
-        if (settings.get(SettingIds.AUTO_LEAVE_RAIDS)) {
-            if (raidListener) return;
+    if (!raidListener) return;
 
-            raidListener = domObserver.on(RAID_BANNER_SELECTOR, (node, isConnected) => this.handleLeave(node, isConnected));
+    raidListener();
+    raidListener = undefined;
+  }
 
-            return;
-        }
+  handleLeave(node, isConnected) {
+    if (!isConnected) return;
 
-        if (!raidListener) return;
+    if (settings.get(SettingIds.AUTO_LEAVE_RAIDS)) {
+      const leaveButton = $(node).find('button > div:contains("Leave")');
 
-        raidListener();
-        raidListener = undefined;
+      if (leaveButton.length) {
+        leaveButton.trigger('click');
+      }
     }
-
-    handleLeave(node, isConnected) {
-        if (!isConnected) return;
-        
-        if (settings.get(SettingIds.AUTO_LEAVE_RAIDS)) {
-            let leaveButton = $(node).find('button > div:contains("Leave")');
-
-            if (leaveButton.length) {
-                leaveButton.click();
-            }
-        }
-    }
+  }
 }
 
 export default new AutoLeaveRaidsModule();
