7aed3e44be6c6c18b4969a15c420d02152919ea0
dclstn <8022520-dclstn@users.noreply.gitlab.com>
2021-07-30 02:06:03 +0100

    virtualized list

--- src/modules/emote_menu/components/Emote.jsx
+++ src/modules/emote_menu/components/Emote.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import LazyLoad from 'react-lazy-load';
+import React, {useEffect, useState} from 'react';
 import styles from '../styles/emotes.module.css';
 
 export default function Emote({code, data, onClick, ...restProps}) {
   if (!data?.images) return null;
+
+  const [loadImage, setLoadImage] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoadImage(true);
+    }, 300);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <button {...restProps} type="button" className={styles.button} onClick={onClick}>
-      <LazyLoad offsetBottom={100}>
-        <img alt={code} src={data.images['1x']} />
-      </LazyLoad>
+      {loadImage && <img alt={code} src={data.images['1x']} />}
     </button>
   );
 }
--- src/modules/emote_menu/grid.js
+++ src/modules/emote_menu/grid.js
@@ -1,15 +1,50 @@
-import Emotes from '../emotes/index.js';
+import watcher from '../../watcher.js';
+import emotes from '../emotes/index.js';
 
-class EmotesGrid extends Emotes {
+const COLOUMN_COUNT = 7;
+
+export default class EmotesGrid {
   constructor() {
-    super();
-    this.cols = 6;
-    console.log(this);
+    this.headers = [];
+    this.rows = [];
+    this.totalCols = COLOUMN_COUNT;
+    this.totalRows = Math.ceil(emotes.length / this.totalCols);
+    this.loadEmotes();
   }
 
-  get totalRows() {
-    return this.length;
+  loadEmotes() {
+    let index = 0;
+
+    this.headers = [];
+    this.rows = [];
+
+    for (const provider of emotes.getAllEmotes()) {
+      if (provider.emotes.size === 0) {
+        continue;
+      }
+
+      this.rows.push(provider.provider);
+      this.headers.push(index);
+      index++;
+
+      const emotesIter = provider.emotes[Symbol.iterator]();
+
+      for (let i = 0; i < this.totalRows; i++) {
+        const row = [];
+        for (let k = 0; k < this.totalCols; k++) {
+          row.push(emotesIter.next().value);
+        }
+        this.rows.push(row);
+        index++;
+      }
+    }
+  }
+
+  getRow(index) {
+    return this.rows[index];
+  }
+
+  isHeader(index) {
+    return this.headers.includes(index);
   }
 }
-
-export default new EmotesGrid();
