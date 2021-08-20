e0844fb7999006144265d6e19e269d011f9f7980
Dylan Ishihara <dylan.ishihara@gmail.com>
2021-08-20 10:17:10 -0700

    creates actual setting toggle (not linked)

--- src/modules/settings/components/settings/PreventChatClear.jsx
+++ src/modules/settings/components/settings/PreventChatClear.jsx
@@ -6,9 +6,23 @@ import {CategoryTypes, SettingIds} from '../../../../constants.js';
 import styles from '../../styles/header.module.css'
 
 function ClearChatBehavior() {
-  return()
+  const [value, setValue] = useStorageState(SettingIds.CLEAR_BEHAVIOR);
+
+  return (
+    <Panel header="Chat Clear Behavior">
+      <div className={styles.toggle}>
+        <p className={styles.description}>Prevents Chat from being cleared (Default: Off)</p>
+        <Toggle checked ={value} onChange={(state) => setValue(state)} />
+      </div>
+
+    </Panel>
+  )
+
 }
 
 export default registerComponent(ClearChatBehavior, {
-
+  settingId: SettingIds.CLEAR_BEHAVIOR,
+  name: 'Clear Command Behavior',
+  category: CategoryTypes.CHAT,
+  keywords: ['clear', 'chat']
 })
