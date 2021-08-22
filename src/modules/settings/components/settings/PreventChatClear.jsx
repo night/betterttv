375f1836b30ff154a7a13cedf81d02f34ab1a702
Dylan Ishihara <dylan.ishihara@gmail.com>
2021-08-22 15:42:27 -0700

    fixes formatting errors in PreventChatClear

--- src/modules/settings/components/settings/PreventChatClear.jsx
+++ src/modules/settings/components/settings/PreventChatClear.jsx
@@ -3,7 +3,7 @@ import Panel from 'rsuite/lib/Panel/index.js';
 import Toggle from 'rsuite/lib/Toggle/index.js';
 import {registerComponent, useStorageState} from '../Store.jsx';
 import {CategoryTypes, SettingIds} from '../../../../constants.js';
-import styles from '../../styles/header.module.css'
+import styles from '../../styles/header.module.css';
 
 function ClearChatBehavior() {
   const [value, setValue] = useStorageState(SettingIds.CLEAR_BEHAVIOR);
@@ -12,17 +12,15 @@ function ClearChatBehavior() {
     <Panel header="Chat Clear Behavior">
       <div className={styles.toggle}>
         <p className={styles.description}>Prevents Chat from being cleared (Default: Off)</p>
-        <Toggle checked ={value} onChange={(state) => setValue(state)} />
+        <Toggle checked={value} onChange={(state) => setValue(state)} />
       </div>
-
     </Panel>
-  )
-
+  );
 }
 
 export default registerComponent(ClearChatBehavior, {
   settingId: SettingIds.CLEAR_BEHAVIOR,
   name: 'Clear Command Behavior',
   category: CategoryTypes.CHAT,
-  keywords: ['clear', 'chat']
-})
+  keywords: ['clear', 'chat'],
+});
