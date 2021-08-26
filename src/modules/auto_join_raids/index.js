2144ca53a08b51268272292f65cc10d5f157355d
bkmak <33822064+bkmak@users.noreply.github.com>
2021-08-26 07:21:34 -0400

    Refactor AutoJoinRaidsModule, remove jQuery dependency

--- src/modules/auto_join_raids/index.js
+++ src/modules/auto_join_raids/index.js
@@ -1,45 +1,56 @@
-import $ from 'jquery';
 import watcher from '../../watcher.js';
 import settings from '../../settings.js';
 import domObserver from '../../observers/dom.js';
 import {SettingIds} from '../../constants.js';
 
 const RAID_BANNER_SELECTOR = '[data-test-selector="raid-banner"]';
-
-let raidListener;
+const RAID_BANNER_BUTTONS_SELECTOR = RAID_BANNER_SELECTOR + ' button';
+const LEAVE_BUTTON_TEXT = 'Leave';
 
 class AutoJoinRaidsModule {
-  constructor() {
-    watcher.on('load.chat', () => this.load());
-    settings.on(`changed.${SettingIds.AUTO_JOIN_RAIDS}`, () => this.load());
-  }
+    raidListener;
 
-  load() {
-    if (!settings.get(SettingIds.AUTO_JOIN_RAIDS)) {
-      if (raidListener) return;
-
-      raidListener = domObserver.on(RAID_BANNER_SELECTOR, (node, isConnected) => this.handleLeave(node, isConnected));
-
-      return;
+    constructor() {
+        watcher.on('load.chat', () => this.load());
     }
 
-    if (!raidListener) return;
-
-    raidListener();
-    raidListener = undefined;
-  }
-
-  handleLeave(node, isConnected) {
-    if (!isConnected) return;
-
-    if (!settings.get(SettingIds.AUTO_JOIN_RAIDS)) {
-      const leaveButton = $(node).find('button > div:contains("Leave")');
-
-      if (leaveButton.length) {
-        leaveButton.trigger('click');
-      }
+    load() {
+        if (!this.raidListener) {
+            this.raidListener = domObserver.on(RAID_BANNER_SELECTOR, () => this.handleRaid());
+        }
     }
-  }
+
+    handleRaid() {
+        let autoJoin = settings.get(SettingIds.AUTO_JOIN_RAIDS);
+
+        if (!autoJoin) {
+            this.leaveRaid();
+        }
+    }
+
+    leaveRaid() {
+        const leaveButton = this.findLeaveButton();
+
+        if (leaveButton) {
+            leaveButton.click();
+        }
+    }
+
+    findLeaveButton() {
+        let elements = this.elementsContainingText(RAID_BANNER_BUTTONS_SELECTOR, LEAVE_BUTTON_TEXT);
+
+        if (elements && elements.length) {
+            return elements[0];
+        }
+    }
+
+    elementsContainingText(selector, text) {
+        let elements = document.querySelectorAll(selector);
+
+        return [].filter.call(elements, function (element) {
+            return RegExp(text).test(element.textContent);
+        });
+    }
 }
 
 export default new AutoJoinRaidsModule();
