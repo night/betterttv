dc45df5100491532d0810a18600e1f90bbac6215
bkmak <33822064+bkmak@users.noreply.github.com>
2021-08-26 07:29:36 -0400

    Linting

--- src/modules/auto_join_raids/index.js
+++ src/modules/auto_join_raids/index.js
@@ -4,53 +4,53 @@ import domObserver from '../../observers/dom.js';
 import {SettingIds} from '../../constants.js';
 
 const RAID_BANNER_SELECTOR = '[data-test-selector="raid-banner"]';
-const RAID_BANNER_BUTTONS_SELECTOR = RAID_BANNER_SELECTOR + ' button';
+const RAID_BANNER_BUTTONS_SELECTOR = `${RAID_BANNER_SELECTOR} button`;
 const LEAVE_BUTTON_TEXT = 'Leave';
 
 class AutoJoinRaidsModule {
-    raidListener;
+  raidListener;
 
-    constructor() {
-        watcher.on('load.chat', () => this.load());
+  constructor() {
+    watcher.on('load.chat', () => this.load());
+  }
+
+  load() {
+    if (!this.raidListener) {
+      this.raidListener = domObserver.on(RAID_BANNER_SELECTOR, () => this.handleRaid());
+    }
+  }
+
+  handleRaid() {
+    const autoJoin = settings.get(SettingIds.AUTO_JOIN_RAIDS);
+
+    if (!autoJoin) {
+      this.leaveRaid();
+    }
+  }
+
+  leaveRaid() {
+    const leaveButton = this.findLeaveButton();
+
+    if (leaveButton) {
+      leaveButton.click();
+    }
+  }
+
+  findLeaveButton() {
+    const elements = this.elementsContainingText(RAID_BANNER_BUTTONS_SELECTOR, LEAVE_BUTTON_TEXT);
+
+    if (elements && elements.length) {
+      return elements[0];
     }
 
-    load() {
-        if (!this.raidListener) {
-            this.raidListener = domObserver.on(RAID_BANNER_SELECTOR, () => this.handleRaid());
-        }
-    }
+    return null;
+  }
 
-    handleRaid() {
-        let autoJoin = settings.get(SettingIds.AUTO_JOIN_RAIDS);
+  elementsContainingText(selector, text) {
+    const elements = document.querySelectorAll(selector);
 
-        if (!autoJoin) {
-            this.leaveRaid();
-        }
-    }
-
-    leaveRaid() {
-        const leaveButton = this.findLeaveButton();
-
-        if (leaveButton) {
-            leaveButton.click();
-        }
-    }
-
-    findLeaveButton() {
-        let elements = this.elementsContainingText(RAID_BANNER_BUTTONS_SELECTOR, LEAVE_BUTTON_TEXT);
-
-        if (elements && elements.length) {
-            return elements[0];
-        }
-    }
-
-    elementsContainingText(selector, text) {
-        let elements = document.querySelectorAll(selector);
-
-        return [].filter.call(elements, function (element) {
-            return RegExp(text).test(element.textContent);
-        });
-    }
+    return [].filter.call(elements, (element) => RegExp(text).test(element.textContent));
+  }
 }
 
 export default new AutoJoinRaidsModule();
