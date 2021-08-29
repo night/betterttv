ee8a68d1722ffb97492b36dd8cc65dbbf1a9a37a
bkmak <33822064+bkmak@users.noreply.github.com>
2021-08-28 23:55:37 -0400

    Reorganize selectors

--- src/modules/auto_join_raids/index.js
+++ src/modules/auto_join_raids/index.js
@@ -4,6 +4,7 @@ import domObserver from '../../observers/dom.js';
 import {SettingIds} from '../../constants.js';
 
 const RAID_BANNER_SELECTOR = '[data-test-selector="raid-banner"]';
+const RAID_LEAVE_BUTTON_SELECTOR = `${RAID_BANNER_SELECTOR} button[class*="ScCoreButtonSecondary"]`;
 
 class AutoJoinRaidsModule {
   raidListener;
@@ -25,7 +26,7 @@ class AutoJoinRaidsModule {
   }
 
   leaveRaid() {
-    const leaveButton = document.querySelectorAll(`${RAID_BANNER_SELECTOR} button[class*="ScCoreButtonSecondary"]`)[0];
+    const leaveButton = document.querySelectorAll(`${RAID_LEAVE_BUTTON_SELECTOR}`)[0];
 
     if (leaveButton) {
       leaveButton.click();
