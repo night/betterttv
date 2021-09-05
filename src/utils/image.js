5ae966725016b74a7f4762328ec6dabea7d4635e
dclstn <8022520-dclstn@users.noreply.gitlab.com>
2021-09-05 22:14:23 +0100

    rem switch in utils image.js

--- src/utils/image.js
+++ src/utils/image.js
@@ -2,13 +2,6 @@
 export function createSrcSet(images) {
   return Object.entries(images)
     .filter(([, url]) => url != null)
-    .map(([size, url]) => {
-      switch (size) {
-        case '3x':
-          return `${url} 4x`;
-        default:
-          return `${url} ${size}`;
-      }
-    })
+    .map(([size, url]) => `${url} ${size}`)
     .join(', ');
 }
