599179c8db8571d7d96a9bfe3142fe20648cd99e
dclstn <8022520-dclstn@users.noreply.gitlab.com>
2021-09-06 16:43:59 +0100

    add old legacy smile

--- src/modules/emote_menu/components/LegacyButton.jsx
+++ src/modules/emote_menu/components/LegacyButton.jsx
@@ -1,18 +1,20 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import Whisper from 'rsuite/lib/Whisper/index.js';
 import Tooltip from 'rsuite/lib/Tooltip/index.js';
 import styles from '../styles/legacy-button.module.css';
-import Icons from './Icons.jsx';
 
 export default function LegacyButton() {
   return (
     <Whisper
       trigger="click"
       placement="auto"
-      speaker={<Tooltip>BetterTTV&apos;s emote-menu now replaces twitch&apos;s default menu.</Tooltip>}>
-      <button type="button" className={styles.button}>
-        {Icons.SMILE}
-      </button>
+      speaker={
+        <Tooltip>
+          This button has moved! Click the other :) button in the chat box to access BetterTTV&apos;s emote menu.
+        </Tooltip>
+      }>
+      <button type="button" className={styles.button} />
     </Whisper>
   );
 }
