b19a47e5342ff4fde8b1ec394c82a013af383ef2
dclstn <8022520-dclstn@users.noreply.gitlab.com>
2021-09-11 13:02:40 +0100

    use throttle lib for sidebar scroll

--- src/modules/emote_menu/components/Sidebar.jsx
+++ src/modules/emote_menu/components/Sidebar.jsx
@@ -2,44 +2,41 @@ import React, {useEffect, useRef} from 'react';
 import Nav from 'rsuite/lib/Nav/index.js';
 import Whisper from 'rsuite/lib/Whisper/index.js';
 import Tooltip from 'rsuite/lib/Tooltip/index.js';
+import {throttle} from 'throttle-debounce';
 import {RowHeight, WindowHeight} from '../../../constants.js';
 import styles from '../styles/sidebar.module.css';
 
 const ITEM_HEIGHT = 42;
 
-let timer;
-
 export default function Sidebar({section, onChange, providers}) {
   const containerRef = useRef(null);
 
-  useEffect(() => {
+  const throttleFunc = throttle(1000, () => {
     if (section.eventKey == null) return;
 
-    clearTimeout(timer);
+    const top = containerRef.current.scrollTop;
+    const index = providers.findIndex((provider) => provider.id === section.eventKey);
+    const depth = index * ITEM_HEIGHT;
 
-    timer = setTimeout(() => {
-      const top = containerRef.current.scrollTop;
-      const index = providers.findIndex((provider) => provider.id === section.eventKey);
-      const depth = index * ITEM_HEIGHT;
+    let newTop;
+    if (depth < top) {
+      newTop = depth;
+    }
+    if (depth + ITEM_HEIGHT > top + WindowHeight) {
+      newTop = depth - WindowHeight + RowHeight;
+    }
+    if (newTop == null) {
+      return;
+    }
 
-      let newTop;
-      if (depth < top) {
-        newTop = depth;
-      }
-      if (depth + ITEM_HEIGHT > top + WindowHeight) {
-        newTop = depth - WindowHeight + RowHeight;
-      }
-      if (newTop == null) {
-        return;
-      }
+    containerRef.current.scrollTo({
+      top: newTop,
+      left: 0,
+      behavior: 'smooth',
+    });
+  });
 
-      containerRef.current.scrollTo({
-        top: depth,
-        left: 0,
-        behavior: 'smooth',
-      });
-    }, 100);
-  }, [section]);
+  useEffect(() => throttleFunc(), [section]);
 
   return (
     <div className={styles.sidebar} ref={containerRef}>
