f099bc7380b55e2cfecd56136e407ecaa9201d8b
Night <night@nightdev.com>
2021-10-14 14:38:10 -0400

    prevent re-occurrence of invalid flag comparisons

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -1,8 +1,20 @@
 export function hasFlag(flags, flag) {
+  if (!Number.isSafeInteger(flags)) {
+    throw new Error('invalid flags');
+  }
+  if (!Number.isSafeInteger(flag)) {
+    throw new Error('invalid flag');
+  }
   return (flags & flag) === flag;
 }
 
 export function setFlag(flags, flag, value) {
+  if (!Number.isSafeInteger(flags)) {
+    throw new Error('invalid flags');
+  }
+  if (!Number.isSafeInteger(flag)) {
+    throw new Error('invalid flag');
+  }
   if (value) {
     return flags | flag;
   }
@@ -10,5 +22,11 @@ export function setFlag(flags, flag, value) {
 }
 
 export function getChangedFlags(oldFlags, newFlags) {
+  if (!Number.isSafeInteger(oldFlags)) {
+    throw new Error('invalid oldFlags');
+  }
+  if (!Number.isSafeInteger(newFlags)) {
+    throw new Error('invalid newFlags');
+  }
   return (oldFlags & ~newFlags) | (newFlags & ~oldFlags);
 }
