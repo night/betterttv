62bbed75392ec1a99ea3ccefe5219678ab08645a
Donnie <donnieacolston@gmail.com>
2022-01-11 02:53:01 +0000

    use uniqBy

--- src/modules/emote_menu/utils/twitch-emotes.js
+++ src/modules/emote_menu/utils/twitch-emotes.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import uniqBy from 'lodash.uniqby';
 import sortBy from 'lodash.sortby';
-import flatten from 'lodash.flatten';
 import twitchApi from '../../../utils/twitch-api.js';
 import Emote from '../../emotes/emote.js';
 import Icons from '../components/Icons.jsx';
