51dec9a6db4af9a8f491919608e9a07eb49d02bb
Donnie <donnieacolston@gmail.com>
2022-01-23 19:45:14 +0000

    remove unused code

--- src/modules/settings/youtube/Settings.jsx
+++ src/modules/settings/youtube/Settings.jsx
@@ -6,11 +6,6 @@ import DropdownButton from './DropdownButton.jsx';
 import styles from './Settings.module.css';
 import {importStoreCallback} from '../components/Settings.jsx';
 
-const BTTV_DROPDOWN_BUTTON_CONTAINER_SELECTOR = 'div[data-a-target="bttv-dropdown-button-container"]';
-const DROPDOWN_MENU_ITEMS_SELECTOR = 'ytd-popup-container #sections #items';
-const ITEMS_SELECTOR = '#items';
-const AVATAR_BUTTON_SELECTOR = '#avatar-btn';
-
 const CHAT_SETTINGS_DROPDOWN_ITEMS_SELECTOR = '#contentWrapper #items';
 const CHAT_SETTINGS_MENU_BUTTON_SELECTOR = '#overflow #button';
 const BTTV_CHAT_DROPDOWN_BUTTON_CONTAINER_SELECTOR = 'div[data-a-target="bttv-chat-dropdown-button-container"]';
@@ -21,23 +16,11 @@ function setHandleOpen(newHandleOpen) {
 }
 
 let mountedPanel;
-let mountedDropdownButton;
 let mountedChatDropdownButton;
-
-let menuItemsListener = null;
 
 export default class SettingsModule {
   constructor() {
     this.load();
-
-    // TODO: re-enable this when we have settings like theatre mode...
-    // domObserver.on(AVATAR_BUTTON_SELECTOR, (node, isConnected) => {
-    //   if (!isConnected) {
-    //     return;
-    //   }
-
-    //   node.addEventListener('click', this.loadDropdownButton);
-    // });
 
     domObserver.on(CHAT_SETTINGS_DROPDOWN_ITEMS_SELECTOR, (node, isConnected) => {
       if (!isConnected) {
@@ -117,55 +100,5 @@ export default class SettingsModule {
 
       mountedChatDropdownButton = dropdownButtonContainer;
     }
-  }
-
-  loadDropdownButton() {
-    if (menuItemsListener != null) return;
-
-    // wait for the menu to finish loading...
-    menuItemsListener = domObserver.on(ITEMS_SELECTOR, (node, isConnected) => {
-      if (!isConnected) {
-        return;
-      }
-
-      menuItemsListener();
-      menuItemsListener = null;
-
-      const dropdownButton = document.querySelector(BTTV_DROPDOWN_BUTTON_CONTAINER_SELECTOR);
-
-      if (dropdownButton == null || dropdownButton.hasAttribute('hidden')) {
-        const items = document.querySelectorAll(DROPDOWN_MENU_ITEMS_SELECTOR)[1];
-
-        if (items == null) {
-          return;
-        }
-
-        const dropdownButtonContainer = document.createElement('div');
-        dropdownButtonContainer.setAttribute('data-a-target', 'bttv-dropdown-button-container');
-        items.appendChild(dropdownButtonContainer);
-
-        if (mountedDropdownButton != null) {
-          ReactDOM.unmountComponentAtNode(mountedDropdownButton);
-        }
-
-        ReactDOM.render(
-          <DropdownButton
-            onClick={() => {
-              handleOpen(true);
-
-              // close the dropdown menu when the modal is opened
-              const avatarButton = document.querySelector(AVATAR_BUTTON_SELECTOR);
-
-              if (avatarButton != null) {
-                avatarButton.click();
-              }
-            }}
-          />,
-          dropdownButtonContainer
-        );
-
-        mountedDropdownButton = dropdownButtonContainer;
-      }
-    });
   }
 }
