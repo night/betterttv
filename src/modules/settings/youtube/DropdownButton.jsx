56657764ed177a77db00d43d7c78f2bfc0f54df6
Donnie <donnieacolston@gmail.com>
2022-01-30 10:04:53 +0000

    youtube settings button update

--- src/modules/settings/youtube/DropdownButton.jsx
+++ src/modules/settings/youtube/DropdownButton.jsx
@@ -1,14 +1,11 @@
-import * as faCog from '@fortawesome/free-solid-svg-icons/faCog';
-import {Icon} from '@rsuite/icons';
 import React from 'react';
-import FontAwesomeSvgIcon from '../../../common/components/FontAwesomeSvgIcon.jsx';
 import styles from './Settings.module.css';
 
 export default function DropdownButton(props) {
   return (
-    <button {...props} className={styles.button} type="button">
-      <Icon className={styles.icon} as={FontAwesomeSvgIcon} fontAwesomeIcon={faCog} />
-      <div>BetterTTV Settings</div>
-    </button>
+    <div {...props} className={styles.button}>
+      <div className={styles.buttonIcon} />
+      <div className={styles.buttonLabel}>BetterTTV Settings</div>
+    </div>
   );
 }
