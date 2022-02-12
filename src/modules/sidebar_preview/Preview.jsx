a5708ed7265f509f1b109cfcd7f91e85fd0ba2a6
dclstn <donnieacolston@gmail.com>
2022-02-12 12:49:05 +0000

    fix css + rem loading state

--- src/modules/sidebar_preview/Preview.jsx
+++ src/modules/sidebar_preview/Preview.jsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames';
 import React, {useState, useEffect} from 'react';
 import styles from './Preview.module.css';
 
@@ -10,32 +9,11 @@ function createPreviewImage(user) {
 
 export default function StreamPreview({username: defaultUsername, usernameCallback}) {
   const [username, setUsername] = useState(defaultUsername);
-  const imageRef = React.useRef(null);
-  const loadingRef = React.useRef(true);
-
-  function handleLoad() {
-    window.requestAnimationFrame(() => {
-      const currentImageRef = imageRef.current;
-      if (currentImageRef == null) {
-        return;
-      }
-
-      loadingRef.current = false;
-      currentImageRef.classList.remove(styles.placeholder);
-    });
-  }
-
   useEffect(() => usernameCallback(setUsername), []);
 
   return (
     <div className={styles.previewContainer}>
-      <img
-        ref={imageRef}
-        className={classNames({[styles.placeholder]: loadingRef.current})}
-        src={createPreviewImage(username)}
-        alt={`${username}-preview`}
-        onLoad={loadingRef.current ? handleLoad : undefined}
-      />
+      <img src={createPreviewImage(username)} alt={`${username}-preview`} />
     </div>
   );
 }
