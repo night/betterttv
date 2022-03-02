1ad08191305a1b20144b5305666bd240f392999e
dclstn <donnieacolston@gmail.com>
2022-03-02 17:08:18 +0000

    Revert "autoReposition"
    This reverts commit 18fecefbb329614e236323090f0e0f7fe1617334.

--- src/modules/emote_autocomplete/components/AutocompletePopover.jsx
+++ src/modules/emote_autocomplete/components/AutocompletePopover.jsx
@@ -1,13 +1,14 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import classNames from 'classnames';
 import Popover from 'rsuite/Popover';
 import mergeRefs from 'react-merge-refs';
 import styles from './AutocompletePopover.module.css';
 import Emotes from './Emotes.jsx';
 import repositionPopover from '../../../utils/popover.js';
-import useAutoRepositionPopout from '../../../common/hooks/AutoRepositionPopover.jsx';
+import useResize from '../../../common/hooks/Resize.jsx';
 
 const TOP_PADDING = 2;
+const DEFAULT_POPOVER_WIDTH = 300;
 
 const EmoteMenuPopover = React.forwardRef(
   (
@@ -25,9 +26,16 @@ const EmoteMenuPopover = React.forwardRef(
     ref
   ) => {
     const localRef = useRef(null);
+    const [popoverWidth, setPopoverWidth] = useState(DEFAULT_POPOVER_WIDTH);
 
     const reposition = () => repositionPopover(localRef, boundingQuerySelector, TOP_PADDING);
-    const [popoverWidth] = useAutoRepositionPopout(reposition, chatInputElement, [localRef, style]);
+    useEffect(() => reposition(), [localRef, style]);
+
+    useResize(() => {
+      const {width} = chatInputElement.getBoundingClientRect();
+      setPopoverWidth(width);
+      reposition();
+    });
 
     return (
       <Popover
