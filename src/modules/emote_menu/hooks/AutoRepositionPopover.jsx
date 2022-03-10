56244899207be604046167c10568d155f0a27d9f
dclstn <donnieacolston@gmail.com>
2022-03-10 00:00:08 +0000

    avoid anon funcs

--- src/modules/emote_menu/hooks/AutoRepositionPopover.jsx
+++ src/modules/emote_menu/hooks/AutoRepositionPopover.jsx
@@ -5,7 +5,9 @@ import repositionPopover from '../../../utils/popover.js';
 const TOP_PADDING = 2;
 
 export default function useAutoPositionPopover(localRef, boundingQuerySelector, dependencies) {
-  const reposition = () => repositionPopover(localRef, boundingQuerySelector, TOP_PADDING);
+  function reposition() {
+    repositionPopover(localRef, boundingQuerySelector, TOP_PADDING);
+  }
 
   useEffect(reposition, dependencies);
   useResize(reposition);
