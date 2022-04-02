89e99bce02ba9765d8a3c986d4743bf0e785b475
dclstn <donnieacolston@gmail.com>
2022-04-02 19:33:38 +0100

    update request resize

--- src/common/hooks/Resize.jsx
+++ src/common/hooks/Resize.jsx
@@ -4,12 +4,12 @@ import debounce from 'lodash.debounce';
 export default function useResize(callback) {
   useEffect(() => {
     function handleResize() {
-      callback();
+      requestAnimationFrame(callback);
       // Twitch animates chat moving on zoom changes
-      setTimeout(() => callback(), 500);
+      setTimeout(() => requestAnimationFrame(callback), 500);
     }
 
-    const requestResize = debounce(() => requestAnimationFrame(handleResize), 250);
+    const requestResize = debounce(handleResize, 250);
 
     window.addEventListener('resize', requestResize);
     return () => window.removeEventListener('resize', requestResize);
