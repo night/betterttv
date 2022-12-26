ee25fbe3e03a1291df2809b0e2c43d79e2c42a9b
philipsdean <59628874+philipsdean@users.noreply.github.com>
2022-12-26 15:41:10 -0600

    [Feature] Add Auto Theme Setting (#4616)

---
+++ src/modules/settings/components/settings/twitch/Theme.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Panel from 'rsuite/Panel';
+import Toggle from 'rsuite/Toggle';
+import {registerComponent} from '../../Store.jsx';
+import {CategoryTypes, SettingIds} from '../../../../../constants.js';
+import styles from '../../../styles/header.module.css';
+import useStorageState from '../../../../../common/hooks/StorageState.jsx';
+
+function Theme() {
+  const [darkThemeValue, setDarkThemeValue] = useStorageState(SettingIds.DARKENED_MODE);
+  const [autoThemeValue, setAutoThemeValue] = useStorageState(SettingIds.AUTO_THEME_MODE);
+
+  return (
+    <Panel header="Theme">
+      <div className={styles.settingRow}>
+        <p className={styles.settingDescription}>Dark theme</p>
+        <Toggle checked={darkThemeValue} onChange={(state) => setDarkThemeValue(state)} disabled={autoThemeValue} />
+      </div>
+      <div className={styles.settingRow}>
+        <p className={styles.settingDescription}>Automatically set dark theme from your system&apos;s theme</p>
+        <Toggle checked={autoThemeValue} onChange={(state) => setAutoThemeValue(state)} />
+      </div>
+    </Panel>
+  );
+}
+
+registerComponent(Theme, {
+  settingId: SettingIds.DARKENED_MODE,
+  name: 'Theme',
+  category: CategoryTypes.CHANNEL,
+  keywords: ['dark', 'mode', 'light', 'theme', 'white', 'black'],
+});
