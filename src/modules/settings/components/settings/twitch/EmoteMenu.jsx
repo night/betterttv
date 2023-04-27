1778dc2aee37735bdb26df59da5f7c0223313528
dclstn <donnieacolston@gmail.com>
2023-04-27 09:35:12 +0100

    reduce coginitive input

--- src/modules/settings/components/settings/twitch/EmoteMenu.jsx
+++ src/modules/settings/components/settings/twitch/EmoteMenu.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Panel from 'rsuite/Panel';
-import Radio from 'rsuite/Radio';
-import RadioGroup from 'rsuite/RadioGroup';
-import FormGroup from 'rsuite/FormGroup';
+import {Toggle} from 'rsuite';
 import {registerComponent} from '../../Store.jsx';
 import {CategoryTypes, SettingIds, EmoteMenuTypes} from '../../../../../constants.js';
 import styles from '../../../styles/header.module.css';
@@ -13,42 +11,26 @@ const SETTING_NAME = formatMessage({defaultMessage: 'Emote Menu'});
 
 function EmoteMenu() {
   const [value, setValue] = useStorageState(SettingIds.EMOTE_MENU);
-
+  const toggled = value !== EmoteMenuTypes.NONE;
   return (
     <Panel header={SETTING_NAME}>
-      <div className={styles.setting}>
+      <div className={styles.settingRow}>
         <p className={styles.settingDescription}>
           {formatMessage({defaultMessage: 'Enables a more advanced emote menu for chat'})}
         </p>
-        <FormGroup controlId="radioList">
-          <RadioGroup name="radioList" value={value} onChange={(state) => setValue(state)}>
-            <Radio key="default" value={EmoteMenuTypes.ENABLED}>
-              <div>
-                <p className={styles.heading}>{formatMessage({defaultMessage: 'Enabled'})}</p>
-                <p className={styles.settingDescription}>
-                  {formatMessage({defaultMessage: 'Replace the default emote picker button'})}
-                </p>
-              </div>
-            </Radio>
-            <Radio key="replace-native" value={EmoteMenuTypes.LEGACY}>
-              <div>
-                <p className={styles.heading}>{formatMessage({defaultMessage: 'Legacy'})}</p>
-                <p className={styles.settingDescription}>
-                  {formatMessage({defaultMessage: 'Emote picker button appears below chat input box'})}
-                </p>
-              </div>
-            </Radio>
-            <Radio key="none" value={EmoteMenuTypes.NONE}>
-              <div>
-                <p className={styles.heading}>{formatMessage({defaultMessage: 'None'})}</p>
-                <p className={styles.settingDescription}>
-                  {formatMessage({defaultMessage: 'Hide the emote picker button entirely'})}
-                </p>
-              </div>
-            </Radio>
-          </RadioGroup>
-        </FormGroup>
+        <Toggle checked={toggled} onChange={(state) => setValue(state ? EmoteMenuTypes.LEGACY : EmoteMenuTypes.NONE)} />
       </div>
+      {toggled ? (
+        <div className={styles.settingRow}>
+          <p className={styles.settingDescription}>
+            {formatMessage({defaultMessage: "Replace twitch's native emote menu."})}
+          </p>
+          <Toggle
+            checked={value === EmoteMenuTypes.ENABLED}
+            onChange={(state) => setValue(state ? EmoteMenuTypes.ENABLED : EmoteMenuTypes.LEGACY)}
+          />
+        </div>
+      ) : null}
     </Panel>
   );
 }
