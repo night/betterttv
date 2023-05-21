81967aebb8db491ae0ce7c6ed69fe4949d4c795a
dclstn <donnieacolston@gmail.com>
2023-05-21 22:41:35 +0100

    final refactor

--- src/modules/emote_menu/twitch/EmoteMenu.jsx
+++ src/modules/emote_menu/twitch/EmoteMenu.jsx
@@ -11,17 +11,15 @@ import twitch from '../../../utils/twitch.js';
 
 const CHAT_TEXT_AREA = 'textarea[data-a-target="chat-input"], div[data-a-target="chat-input"]';
 
+// For legacy button
+const LEGACY_BTTV_EMOTE_PICKER_BUTTON_CONTAINER_SELECTOR =
+  'div[data-a-target="legacy-bttv-emote-picker-button-container"]';
 const CHAT_SETTINGS_BUTTON_CONTAINER_SELECTOR = '.chat-input div[data-test-selector="chat-input-buttons-container"]';
+
+const BTTV_EMOTE_PICKER_BUTTON_CONTAINER_SELECTOR = 'div[data-a-target="bttv-emote-picker-button-container"]';
 const EMOTE_PICKER_BUTTON_SELECTOR = 'button[data-a-target="emote-picker-button"]';
-const BTTV_BUTTON_CONTAINER_ID = 'bttv-emote-menu-button-container';
-const BTTV_BUTTON_CONTAINER_SELECTOR = `#${BTTV_BUTTON_CONTAINER_ID}`;
 
 class SafeEmoteMenuButton extends React.Component {
-  componentDidMount() {
-    const {onMount} = this.props;
-    onMount();
-  }
-
   componentDidCatch(error, info) {
     const {onError} = this.props;
     onError(error, info);
@@ -37,7 +35,109 @@ class SafeEmoteMenuButton extends React.Component {
 }
 
 let mountedRoot;
-let isMounted = false;
+let legacyMountedRoot;
+
+function appendToChat({code: text}, shouldFocus = true) {
+  let prefixText = twitch.getChatInputValue();
+
+  // suffix the prefix with a space if it needs one
+  if (prefixText.length > 0 && !prefixText.endsWith(' ')) {
+    prefixText += ' ';
+  }
+
+  twitch.setChatInputValue(`${prefixText}${text} `, shouldFocus);
+}
+
+function unloadLegacyButton(legacyContainer) {
+  if (legacyContainer === undefined) {
+    legacyContainer = document.querySelector(LEGACY_BTTV_EMOTE_PICKER_BUTTON_CONTAINER_SELECTOR);
+  }
+  if (legacyContainer != null) {
+    legacyContainer.remove();
+  }
+  if (legacyMountedRoot != null) {
+    legacyMountedRoot.unmount();
+  }
+}
+
+function loadLegacyButton() {
+  const legacyContainer = document.querySelector(LEGACY_BTTV_EMOTE_PICKER_BUTTON_CONTAINER_SELECTOR);
+  if (legacyContainer != null) {
+    return;
+  }
+
+  const container = document.querySelector(CHAT_SETTINGS_BUTTON_CONTAINER_SELECTOR);
+  if (container == null) {
+    return;
+  }
+
+  const rightContainer = container.lastChild;
+  const buttonContainer = document.createElement('div');
+  buttonContainer.setAttribute('data-a-target', 'legacy-bttv-emote-picker-button-container');
+  rightContainer.insertBefore(buttonContainer, rightContainer.lastChild);
+
+  if (legacyMountedRoot != null) {
+    legacyMountedRoot.unmount();
+  }
+
+  legacyMountedRoot = createRoot(buttonContainer);
+  legacyMountedRoot.render(
+    <SafeEmoteMenuButton
+      onError={() => unloadLegacyButton(legacyContainer)}
+      appendToChat={appendToChat}
+      className={styles.button}
+      boundingQuerySelector={CHAT_TEXT_AREA}
+    />
+  );
+}
+
+function unloadButton(container, chatInputIcons) {
+  if (container === undefined) {
+    container = document.querySelector(BTTV_EMOTE_PICKER_BUTTON_CONTAINER_SELECTOR);
+  }
+  if (chatInputIcons === undefined) {
+    chatInputIcons = document.querySelector(EMOTE_PICKER_BUTTON_SELECTOR)?.parentElement?.parentElement;
+  }
+  if (chatInputIcons != null) {
+    chatInputIcons.classList.remove(styles.chatInputIcon);
+  }
+  if (container != null) {
+    container.remove();
+  }
+  if (mountedRoot != null) {
+    mountedRoot.unmount();
+  }
+}
+
+function loadButton() {
+  const container = document.querySelector(BTTV_EMOTE_PICKER_BUTTON_CONTAINER_SELECTOR);
+  if (container != null) {
+    return;
+  }
+  const chatInputIcons = document.querySelector(EMOTE_PICKER_BUTTON_SELECTOR)?.parentElement?.parentElement;
+  if (chatInputIcons == null) {
+    return;
+  }
+
+  const buttonContainer = document.createElement('div');
+  buttonContainer.setAttribute('data-a-target', 'bttv-emote-picker-button-container');
+  chatInputIcons.classList.add(styles.chatInputIcon);
+  chatInputIcons.appendChild(buttonContainer);
+
+  if (mountedRoot != null) {
+    mountedRoot.unmount();
+  }
+
+  mountedRoot = createRoot(buttonContainer);
+  mountedRoot.render(
+    <SafeEmoteMenuButton
+      onError={() => unloadButton(buttonContainer, chatInputIcons)}
+      appendToChat={appendToChat}
+      className={styles.button}
+      boundingQuerySelector={CHAT_TEXT_AREA}
+    />
+  );
+}
 
 export default class EmoteMenuModule {
   constructor() {
@@ -46,93 +146,30 @@ export default class EmoteMenuModule {
         return;
       }
 
-      this.loadButton();
+      this.load();
     });
-    watcher.on('load.chat', () => this.loadButton());
-    settings.on(`changed.${SettingIds.EMOTE_MENU}`, () => this.loadButton());
+    watcher.on('load.chat', () => this.load());
+    settings.on(`changed.${SettingIds.EMOTE_MENU}`, () => this.load());
   }
 
-  loadButton() {
+  load() {
     if (getCurrentUser() == null) {
       return;
     }
 
-    const emoteMenuValue = settings.get(SettingIds.EMOTE_MENU);
-    if (emoteMenuValue !== EmoteMenuTypes.NONE) {
-      if (isMounted && mountedRoot != null) {
-        mountedRoot.unmount();
-        isMounted = false;
-        const currentContainer = document.querySelector(BTTV_BUTTON_CONTAINER_SELECTOR);
-        if (currentContainer != null) {
-          currentContainer.remove();
-        }
-      }
-
-      const container = document.createElement('div');
-      container.setAttribute('id', BTTV_BUTTON_CONTAINER_ID);
-
-      if (emoteMenuValue === EmoteMenuTypes.ENABLED) {
-        const nativeEmotePickerButtonContainer = document.querySelector(EMOTE_PICKER_BUTTON_SELECTOR)?.parentElement;
-        if (nativeEmotePickerButtonContainer == null) {
-          return;
-        }
-        const chatInputIcons = nativeEmotePickerButtonContainer.parentElement;
-        if (chatInputIcons == null) {
-          return;
-        }
-        chatInputIcons.classList.add(styles.chatInputIcon);
-        chatInputIcons.appendChild(container);
-      } else {
-        const chatSettingsButtonContainer = document.querySelector(CHAT_SETTINGS_BUTTON_CONTAINER_SELECTOR);
-        if (chatSettingsButtonContainer == null) {
-          return;
-        }
-        const rightContainer = chatSettingsButtonContainer.lastChild;
-        rightContainer.insertBefore(container, rightContainer.lastChild);
-      }
-
-      mountedRoot = createRoot(container);
-      mountedRoot.render(
-        <SafeEmoteMenuButton
-          onError={() => this.show(EmoteMenuTypes.NONE)}
-          onMount={() => {
-            this.show(emoteMenuValue);
-            isMounted = true;
-          }}
-          appendToChat={this.appendToChat}
-          className={styles.button}
-          boundingQuerySelector={CHAT_TEXT_AREA}
-        />
-      );
+    switch (settings.get(SettingIds.EMOTE_MENU)) {
+      case EmoteMenuTypes.ENABLED:
+        unloadLegacyButton();
+        loadButton();
+        break;
+      case EmoteMenuTypes.LEGACY_ENABLED:
+        loadLegacyButton();
+        unloadButton();
+        break;
+      default:
+        unloadLegacyButton();
+        unloadButton();
+        break;
     }
-
-    if (isMounted) {
-      this.show(emoteMenuValue);
-    }
-  }
-
-  show(emoteMenuValue) {
-    const container = document.querySelector(BTTV_BUTTON_CONTAINER_SELECTOR);
-    container.classList.toggle(styles.hideEmoteMenuButton, emoteMenuValue === EmoteMenuTypes.NONE);
-
-    const emotePickerButtonContainer = document.querySelector(EMOTE_PICKER_BUTTON_SELECTOR)?.parentElement;
-    if (emotePickerButtonContainer == null) {
-      return;
-    }
-    emotePickerButtonContainer.classList.toggle(
-      styles.hideDefaultEmoteMenuButton,
-      emoteMenuValue === EmoteMenuTypes.ENABLED
-    );
-  }
-
-  appendToChat({code: text}, shouldFocus = true) {
-    let prefixText = twitch.getChatInputValue();
-
-    // suffix the prefix with a space if it needs one
-    if (prefixText.length > 0 && !prefixText.endsWith(' ')) {
-      prefixText += ' ';
-    }
-
-    twitch.setChatInputValue(`${prefixText}${text} `, shouldFocus);
   }
 }
