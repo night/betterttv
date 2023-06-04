71a44e5a8e767d3ffa5ffb2c13e521e88253d8d6
Donnie <donnieacolston@gmail.com>
2023-06-04 11:12:31 +0100

    Normalize path fix

--- dev/webpack-import-glob.cjs
+++ dev/webpack-import-glob.cjs
@@ -14,7 +14,7 @@ function replacer(match, quote, filename) {
         try {
           await import(${quote}${normalizePath(file)}${quote});
         } catch (e) {
-          debug.error('Failed to import ${file}', e.stack);
+          debug.error('Failed to import ${normalizePath(file)}', e.stack);
         }
       `
     )
