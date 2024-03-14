db224d899e9e039728046d522bb2b1e110738556
Donnie <donnieacolston@gmail.com>
2024-03-14 14:32:02 +0000

    update sidebar module

--- src/modules/hide_sidebar_elements/index.js
+++ src/modules/hide_sidebar_elements/index.js
@@ -5,94 +5,85 @@ import {hasFlag} from '../../utils/flags.js';
 import {loadModuleForPlatforms} from '../../utils/modules.js';
 import twitch from '../../utils/twitch.js';
 import watcher from '../../watcher.js';
+import styles from './styles.module.css';
 
-const sectionObserverRemovers = {};
+const sidebarSectionSelector = '.side-nav-section';
+const offlineChannelSelector = '.side-nav-card > .side-nav-card__link--offline';
+
+let offlineChannelObserverRemover = null;
+
+function handleSidebarSection(node) {
+  const sidebarSection = twitch.getSidebarSection(node);
+  if (sidebarSection == null) {
+    return;
+  }
+
+  const setting = settings.get(SettingIds.SIDEBAR);
+  switch (sidebarSection.type) {
+    case 'RECENTLY_VISITED_SECTION': {
+      node.classList.toggle(styles.hide, !hasFlag(setting, SidebarFlags.RECENTLY_WATCHED_CHANNELS));
+      break;
+    }
+    case 'RECOMMENDED_SECTION': {
+      node.classList.toggle(styles.hide, !hasFlag(setting, SidebarFlags.RECOMMENDED_CHANNELS));
+      break;
+    }
+    case 'SIMILAR_SECTION': {
+      node.classList.toggle(styles.hide, !hasFlag(setting, SidebarFlags.SIMILAR_CHANNELS));
+      break;
+    }
+    default: {
+      return;
+    }
+  }
+}
 
 class HideSidebarElementsModule {
   constructor() {
     settings.on(`changed.${SettingIds.SIDEBAR}`, () => {
-      this.toggleRecentlyWatchedChannels();
-      this.toggleRecommendedChannels();
-      this.toggleSimilarChannels();
+      this.updateAllSidebarSections();
+      this.loadOfflineChannelObserver();
       this.toggleAutoExpandChannels();
-      this.toggleOfflineFollowedChannels();
     });
     watcher.on('load', () => {
-      this.toggleRecentlyWatchedChannels();
-      this.toggleRecommendedChannels();
-      this.toggleSimilarChannels();
+      this.updateAllSidebarSections();
+      this.loadOfflineChannelObserver();
       this.toggleAutoExpandChannels();
-      this.toggleOfflineFollowedChannels();
+    });
+    domObserver.on(sidebarSectionSelector, (node, isConnected) => {
+      if (!isConnected) return;
+      handleSidebarSection(node);
     });
   }
 
-  toggleChannels(flag, hideClass, selector, section) {
-    if (!hasFlag(settings.get(SettingIds.SIDEBAR), flag)) {
-      if (sectionObserverRemovers[flag] == null) {
-        sectionObserverRemovers[flag] = domObserver.on(
-          selector,
-          (node, isConnected) => {
-            if (!isConnected) {
-              return;
-            }
+  loadOfflineChannelObserver() {
+    const setting = settings.get(SettingIds.SIDEBAR);
+    const shouldHideOfflineChannels = !hasFlag(setting, SidebarFlags.OFFLINE_FOLLOWED_CHANNELS);
 
-            const sidebarSection = twitch.getSidebarSection(node);
-            if (section && section !== sidebarSection?.type) {
-              return;
-            }
+    if (shouldHideOfflineChannels && offlineChannelObserverRemover == null) {
+      offlineChannelObserverRemover = domObserver.on(offlineChannelSelector, (node, isConnected) => {
+        if (!isConnected) {
+          return;
+        }
+        node.classList.add(styles.hideOfflineChannel);
+      });
+    }
 
-            node.classList.add(hideClass);
-          },
-          {useParentNode: true}
-        );
+    if (!shouldHideOfflineChannels && offlineChannelObserverRemover != null) {
+      offlineChannelObserverRemover();
+      offlineChannelObserverRemover = null;
+      const nodes = document.getElementsByClassName(styles.hideOfflineChannel);
+      for (const node of nodes) {
+        node.classList.remove(styles.hideOfflineChannel);
       }
-
-      return;
     }
+  }
 
-    if (sectionObserverRemovers[flag] == null) {
-      return;
+  updateAllSidebarSections() {
+    const sidebarSections = document.querySelectorAll(sidebarSectionSelector);
+    for (const section of sidebarSections) {
+      handleSidebarSection(section);
     }
-
-    Array.from(document.getElementsByClassName(hideClass)).forEach((el) => el.classList.remove(hideClass));
-
-    sectionObserverRemovers[flag]();
-    delete sectionObserverRemovers[flag];
-  }
-
-  toggleRecentlyWatchedChannels() {
-    this.toggleChannels(
-      SidebarFlags.RECENTLY_WATCHED_CHANNELS,
-      'bttv-hide-recently-watched-channels',
-      '.side-nav-section a[data-a-id^="recently-watched-channel"], .side-nav-card[data-test-selector="side-nav-card-collapsed"]',
-      'RECENTLY_VISITED_SECTION'
-    );
-  }
-
-  toggleRecommendedChannels() {
-    this.toggleChannels(
-      SidebarFlags.RECOMMENDED_CHANNELS,
-      'bttv-hide-recommended-channels',
-      '.side-nav-section a[data-test-selector="recommended-channel"], .side-nav-card[data-test-selector="side-nav-card-collapsed"]',
-      'RECOMMENDED_SECTION'
-    );
-  }
-
-  toggleSimilarChannels() {
-    this.toggleChannels(
-      SidebarFlags.SIMILAR_CHANNELS,
-      'bttv-hide-similar-channels',
-      '.side-nav-section a[data-test-selector="similarity-channel"], .side-nav-card[data-test-selector="side-nav-card-collapsed"]',
-      'SIMILAR_SECTION'
-    );
-  }
-
-  toggleOfflineFollowedChannels() {
-    this.toggleChannels(
-      SidebarFlags.OFFLINE_FOLLOWED_CHANNELS,
-      'bttv-hide-followed-offline',
-      '.side-nav-card .side-nav-card__avatar--offline'
-    );
   }
 
   toggleAutoExpandChannels() {
