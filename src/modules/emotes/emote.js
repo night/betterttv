67750832c229f5b32ac66ba1b23470c75d96f9a9
Zack <night@users.noreply.github.com>
2024-06-10 11:55:16 -0400

    Update emote.js

--- src/modules/emotes/emote.js
+++ src/modules/emotes/emote.js
@@ -79,12 +79,9 @@ export default class Emote {
     }
     container.appendChild(image);
 
-    const enlargedImage = new Image();
-    enlargedImage.classList.add('bttv-emote-image-enlarged');
-    enlargedImage.src = createSrc(this.images, shouldRenderStatic, '4x');
-    enlargedImage.alt = `${prefixModifiers != null && prefixModifiers.length > 0 ? `${prefixModifiers.join(' ')} ` : ''}${
-      this.code
-    }${suffixModifiers != null && suffixModifiers.length > 0 ? ` ${suffixModifiers.join(' ')}` : ''}`;
+    const tooltipImage = new Image();
+    tooltipImage.classList.add('bttv-tooltip-emote-image');
+    tooltipImage.src = createSrc(this.images, shouldRenderStatic, '4x');
 
     const tooltipText = document.createElement('div');
     tooltipText.textContent = `${this.code}\n${
@@ -93,7 +90,7 @@ export default class Emote {
 
     const tooltip = document.createElement('div');
     tooltip.classList.add('bttv-tooltip', 'bttv-tooltip--up', 'bttv-tooltip--align-center');
-    tooltip.appendChild(enlargedImage);
+    tooltip.appendChild(tooltipImage);
     tooltip.appendChild(tooltipText);
 
     container.appendChild(tooltip);
