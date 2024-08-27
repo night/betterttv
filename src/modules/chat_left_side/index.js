ad21d3ef7c9b5b247e469f07e22008407766d528
Donnie <donnieacolston@gmail.com>
2024-08-27 12:51:20 +0100

    add missing var

--- src/modules/chat_left_side/index.js
+++ src/modules/chat_left_side/index.js
@@ -15,7 +15,7 @@ class ChatLeftSide {
   toggleLeftSideChat() {
     document.body.classList.toggle('bttv-swap-chat', settings.get(SettingIds.CHAT_LAYOUT) === ChatLayoutTypes.LEFT);
 
-    const sideNav = document.querySelector('.side-nav')?.parentElement;
+    const sideNav = document.querySelector(SIDE_NAV_SELECTOR)?.parentElement;
     const main = document.querySelector('.twilight-main');
     if (sideNav != null && main != null && sideNav.parentElement === main.parentElement) {
       sideNav.classList.add('bttv-side-nav');
