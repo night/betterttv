dbe1422f5ea3ecda3c77a462073ebce1374ad816
Night <night@nightdev.com>
2024-09-01 13:40:45 -0400

    use :has

--- src/modules/chat_left_side/index.js
+++ src/modules/chat_left_side/index.js
@@ -1,25 +1,15 @@
 import {ChatLayoutTypes, PlatformTypes, SettingIds} from '../../constants.js';
-import dom from '../../observers/dom.js';
 import settings from '../../settings.js';
 import {loadModuleForPlatforms} from '../../utils/modules.js';
-
-const SIDE_NAV_SELECTOR = '.side-nav';
 
 class ChatLeftSide {
   constructor() {
     settings.on(`changed.${SettingIds.CHAT_LAYOUT}`, () => this.toggleLeftSideChat());
-    dom.on(SIDE_NAV_SELECTOR, () => this.toggleLeftSideChat());
     this.toggleLeftSideChat();
   }
 
   toggleLeftSideChat() {
     document.body.classList.toggle('bttv-swap-chat', settings.get(SettingIds.CHAT_LAYOUT) === ChatLayoutTypes.LEFT);
-
-    const sideNav = document.querySelector(SIDE_NAV_SELECTOR)?.parentElement;
-    const main = document.querySelector('.twilight-main');
-    if (sideNav != null && main != null && sideNav.parentElement === main.parentElement) {
-      sideNav.classList.add('bttv-side-nav');
-    }
   }
 }
 
