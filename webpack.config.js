694ab10661a1a3de1dad831f6cab27f936b5bbdd
Donnie <donnieacolston@gmail.com>
2025-05-12 17:47:34 +0100

    uncommit changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ import {createRequire} from 'module';
 import path from 'path';
 import {sentryWebpackPlugin} from '@sentry/webpack-plugin';
 import {CleanWebpackPlugin} from 'clean-webpack-plugin';
+// eslint-disable-next-line import/default
 import CopyPlugin from 'copy-webpack-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 import FileManagerPlugin from 'filemanager-webpack-plugin';
