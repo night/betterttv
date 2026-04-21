5cfe00d73d2da46f72a5030906764c0f8291b9c9
Donnie <donnieacolston@gmail.com>
2026-04-22 00:45:16 +0100

    add button

--- src/modules/emote_menu/components/Header.jsx
+++ src/modules/emote_menu/components/Header.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import formatMessage from '../../../i18n/index.js';
-import {CloseButton, TextInput} from '@mantine/core';
+import {ActionIcon, CloseButton, TextInput} from '@mantine/core';
 import styles from './Header.module.css';
 import classNames from 'classnames';
 import {useFocusTrap} from '@mantine/hooks';
+import LogoIcon from '../../../common/components/LogoIcon.jsx';
+import settings from '../../settings/index.js';
 
 function Header({value, opened, onChange, toggleWhisper, selected, className, ...props}) {
   const focusRef = useFocusTrap(opened);
+
+  const handleLogoClick = useCallback(() => {
+    settings.openSettings();
+    toggleWhisper();
+  }, [toggleWhisper]);
+
   return (
     <div className={classNames(styles.header, className)} {...props}>
+      <button color="primary" variant="subtle" className={styles.logoButton} onClick={handleLogoClick}>
+        <LogoIcon className={styles.logoIcon} />
+      </button>
       <TextInput
         ref={focusRef}
-        size="lg"
+        size="md"
         placeholder={selected == null ? formatMessage({defaultMessage: 'Search for Emotes'}) : selected.code}
         value={value}
         onChange={({target: {value}}) => onChange(value)}
